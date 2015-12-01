52fe955ada49960ad095e4d53345e9b2e2cf6b2d
Richard Otis <richard.otis@outlook.com>
2015-12-01 16:23:34 -0500

    Debugging the hd5 error

--- run.py
+++ run.py
@@ -20,7 +20,7 @@ def main(parameters):
   trace_path = os.path.join(parameters['results_path'],
 			    'output-traces', parameters['sumatra_label'],
 			    'traces.hdf5')
-  MDL = pymc.MCMC(mod, db='hdf5', dbname=trace_path,
+  MDL = pymc.MCMC(mod, db='hdf5', dbname=str(trace_path),
                   dbcomplevel=4, dbcomplib='bzip2')
   MDL.sample(**parameters['mcmc'])
 
