08aeafa0862a0816e653c5120c360e66e578d7e2
Richard Otis <richard.otis@outlook.com>
2015-12-02 11:05:41 -0500

    run.py: Add CSV parameter summary

--- run.py
+++ run.py
@@ -6,11 +6,14 @@ from corner import corner
 import tables
 import matplotlib.pyplot as plt
 import pymc
+from pymc.database.base import batchsd
+from pymc import utils
 import numpy as np
 import sys
 import os
 import time
 import glob
+import csv
 from collections import OrderedDict
 from itertools import chain
 
@@ -39,10 +42,12 @@ def main(parameters, seed):
 
 
 def analyze(parameters):
-  trace_file = str(os.path.join('Data', parameters['sumatra_label'], 'traces.hdf5'))
   image_path = os.path.join('Data', parameters['sumatra_label'])
+  # Save traces
+  trace_file = str(os.path.join('Data', parameters['sumatra_label'], 'traces.hdf5'))
   data_dict = OrderedDict()
   with tables.open_file(trace_file, mode='r') as data:
+    os.makedirs(os.path.join(image_path, 'acf'))
     parnames = [x for x in data.root.chain0.PyMCsamples.colnames
 		if not x.startswith('Metropolis') and x != 'deviance']
     for param in sorted(parnames):
@@ -51,7 +56,7 @@ def analyze(parameters):
 	figure = plt.figure()
 	figure.gca().plot(autocorr(trace))
 	figure.gca().set_title(param+' Autocorrelation')
-	figure.savefig(str(os.path.join(image_path, param+'-acf.png')))
+	figure.savefig(str(os.path.join(image_path, 'acf', param+'.png')))
 	plt.close(figure)
 
   data = np.vstack(chain([i for i in data_dict.values()])).T
@@ -62,6 +67,26 @@ def analyze(parameters):
 		  show_titles=True, title_args={"fontsize": 40}, rasterized=True)
   figure.savefig(str(os.path.join(image_path, 'cornerplot.png')))
   plt.close(figure)
+  # Write CSV file with parameter summary (should be close to pymc's format)
+  with open(str(os.path.join(image_path, 'parameters.csv')), 'w') as csvfile:
+    fieldnames = ['Parameter', 'Mean', 'SD', 'Lower 95% HPD', 'Upper 95% HPD',
+		  'MC error', 'q2.5', 'q25', 'q50', 'q75', 'q97.5']
+    writer = csv.DictWriter(csvfile, fieldnames)
+    writer.writeheader()
+    for parname, trace in data_dict.items():
+      qxx = utils.quantiles(trace, qlist=(2.5, 25, 50, 75, 97.5))
+      q2d5, q25, q50, q75, q975 = qxx[2.5], qxx[25], qxx[50], qxx[75], qxx[97.5]
+      lower_hpd, upper_hpd = utils.hpd(trace, 0.05)
+      row = {
+	      'Parameter': parname,
+	      'Mean': trace.mean(0),
+	      'SD': trace.std(0),
+	      'Lower 95% HPD': lower_hpd,
+	      'Upper 95% HPD': upper_hpd,
+	      'MC error': batchsd(trace, min(len(trace), 100)),
+	      'q2.5': q2d5, 'q25': q25, 'q50': q50, 'q75': q75, 'q97.5': q975
+	    }
+      writer.writerow(row)
 
 parameter_file = sys.argv[1]
 parameters = build_parameters(parameter_file)
@@ -69,9 +94,10 @@ parameters = build_parameters(parameter_file)
 project = load_project()
 record = project.new_record(parameters=parameters,
                             main_file=__file__,
-                            reason="reason for running this simulation")
+                            reason="Single-phase fitting tests using Sumatra")
 # Add some tags related to the phases and components present
 record.tags = set([str(i) for i in parameters['phases']+parameters['components']])
+record.tags |= set(['CALPHAD'])
 # Fix a random seed in case we need it again
 seed = parameters.as_dict().get('seed', np.random.randint(0, 1e5))
 parameters.update({"sumatra_label": record.label, "seed": seed})
