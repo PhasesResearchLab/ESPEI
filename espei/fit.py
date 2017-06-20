98552944f7dae78ffa46404f9790843b5bba8245
bocklund <brandonbocklund@gmail.com>
2017-06-20 07:25:16 -0700

    ENH:Add verbosity arg to cmd line

--- espei/fit.py
+++ espei/fit.py
@@ -19,6 +19,13 @@ from espei.utils import ImmediateClient, load_datasets, recursive_glob
 parser = argparse.ArgumentParser(description=__doc__)
 
 parser.add_argument(
+    "-v", "--verbose",
+    action="count",
+    default=0,
+    help="Set the logging verbosity. Stacks up to two times (-vv)."
+    )
+
+parser.add_argument(
     "--dask-scheduler",
     metavar="HOST:PORT",
     help="Host and port of dask distributed scheduler")
@@ -60,11 +67,20 @@ parser.add_argument(
 
 def main():
     args = parser.parse_args(sys.argv[1:])
+    # handle verbosity
+    verbosity = {0: logging.WARNING,
+                 1: logging.INFO,
+                 2: logging.DEBUG}
+    user_verbosity = args.verbose if args.verbose < 2 else 2
+    logging.basicConfig(level=verbosity[user_verbosity])
+    # create the scheduler if not passed
     if not args.dask_scheduler:
         pass
         args.dask_scheduler = LocalCluster(n_workers=int(multiprocessing.cpu_count()/2), threads_per_worker=1, processes=True)
     client = ImmediateClient(args.dask_scheduler)
     logging.info("Running with dask scheduler: %s [%s cores]" % (args.dask_scheduler, sum(client.ncores().values())))
+    logging.info("bokeh server for dask scheduler at localhost:{}".format(client.scheduler_info()['services']['bokeh']))
+    # load datasets and handle i/o
     datasets = load_datasets(sorted(recursive_glob(args.datasets, '*.json')))
     recfile = open(args.iter_record, 'a') if args.iter_record else None
     tracefile = args.tracefile if args.tracefile else None
@@ -72,9 +88,8 @@ def main():
         resume = Database(args.input_tdb)
     else:
         resume = None
-    
+    # fitting
     dbf, sampler, parameters = fit(args.fit_settings, datasets, scheduler=client, recfile=recfile, tracefile=tracefile, resume=resume)
-
     dbf.to_file(args.output_tdb, if_exists='overwrite')
 
 if __name__ == '__main__':
