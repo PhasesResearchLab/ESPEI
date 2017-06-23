b74faf813ec81310a8b5aea65e45a48be8fada3c
bocklund <brandonbocklund@gmail.com>
2017-06-23 12:40:56 -0700

    DOC: releasing update versioning

--- espei/_version.py
+++ espei/_version.py
@@ -41,7 +41,7 @@ def get_config():
     cfg = VersioneerConfig()
     cfg.VCS = "git"
     cfg.style = "pep440"
-    cfg.tag_prefix = "espei"
+    cfg.tag_prefix = ""
     cfg.parentdir_prefix = "espei-"
     cfg.versionfile_source = "espei/_version.py"
     cfg.verbose = False
