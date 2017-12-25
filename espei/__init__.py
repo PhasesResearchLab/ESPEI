36d4ac8825d2c31d167e0dcf84e96b75224ab263
bocklund <brandonbocklund@gmail.com>
2017-12-25 14:23:26 -0500

    ENH: Silence NumPy divide by zero warnings

--- espei/__init__.py
+++ espei/__init__.py
@@ -31,3 +31,4 @@ import warnings
 warnings.filterwarnings('ignore', message='Mean of empty slice')
 warnings.filterwarnings('ignore', message='invalid value encountered in subtract')
 warnings.filterwarnings('ignore', message='invalid value encountered in greater')
+warnings.filterwarnings('ignore', message='divide by zero encountered')
