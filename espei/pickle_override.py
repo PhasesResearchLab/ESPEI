3267871e0cb4bd3a51a84b11c9b8151429e42760
bocklund <brandonbocklund@gmail.com>
2018-03-22 23:28:31 -0400

    MAINT: Remove dead code pickle_override module

--- espei/pickle_override.py
+++
@@ -1,9 +0,0 @@
-from pickle import *
-from pickle import _Unpickler, whichmodule
-import dill
-import cloudpickle
-
-dumps = cloudpickle.dumps
-
-_Pickler = cloudpickle.CloudPickler
-Pickler = cloudpickle.CloudPickler
