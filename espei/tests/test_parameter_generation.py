cd0d5d8d8cc1681f2b31a228d150babef12da5a9
Brandon Bocklund <brandonbocklund@gmail.com>
2018-04-17 10:24:00 -0400

    FIX: Fix parameter generation for phases with vacancies (#61)
    Fix paramselect bug where vacancy species were not getting subtracted from the moles per formula unit. Includes a test to prevent future regressions.

--- espei/tests/test_parameter_generation.py
+++ espei/tests/test_parameter_generation.py
@@ -44,6 +44,7 @@ def test_mixing_energies_are_fit(datasets_db):
     assert dbf.elements == {'AL', 'B'}
     assert set(dbf.phases.keys()) == {'LIQUID', 'FCC_A1'}
     assert len(dbf._parameters.search(where('parameter_type') == 'L')) == 1
+    assert dbf.symbols['VV0000'] == -40000
 
     # check that read/write is ok
     read_dbf = dbf.from_string(dbf.to_string(fmt='tdb'), fmt='tdb')
@@ -54,6 +55,45 @@ def test_mixing_energies_are_fit(datasets_db):
     # the error should be exactly 0 because we are only fitting to one point
     from espei.error_functions import calculate_thermochemical_error
     assert calculate_thermochemical_error(read_dbf, sorted(dbf.elements), sorted(dbf.phases.keys()), datasets_db) == 0
+
+
+def test_multi_sublattice_mixing_energies_are_fit(datasets_db):
+    """Tests the correct excess parameter is fit for phases with multiple sublattices with vacancies."""
+    phase_models = {
+        "components": ["AL", "B", "VA"],
+        "phases": {
+            "FCC_A1" : {
+                "sublattice_model": [["AL", "B"], ["AL", "VA"]],
+                "sublattice_site_ratios": [1, 3]
+            }
+        }
+    }
+
+    dataset_excess_mixing = {
+        "components": ["AL", "B", "VA"],
+        "phases": ["FCC_A1"],
+        "solver": {
+            "sublattice_site_ratios": [1, 3],
+            "sublattice_occupancies": [[[0.5, 0.5], 1], [[0.5, 0.5], 1]],
+            "sublattice_configurations": [[["AL", "B"], "VA"], [["AL", "B"], "AL"]],
+            "mode": "manual"
+        },
+        "conditions": {
+            "P": 101325,
+            "T": 298.15
+        },
+        "output": "HM_MIX",
+        "values": [[[-10000, -10000]]]
+    }
+    datasets_db.insert(dataset_excess_mixing)
+
+    dbf = generate_parameters(phase_models, datasets_db, 'SGTE91', 'linear')
+
+    assert set(dbf.phases.keys()) == {'FCC_A1'}
+    assert len(dbf._parameters.search(where('parameter_type') == 'L')) == 2
+    assert dbf.symbols['VV0000'] == -160000.0
+    assert dbf.symbols['VV0001'] == -40000.0
+
 
 def test_sgte_reference_state_naming_is_correct_for_character_element(datasets_db):
     """Elements with single character names should get the correct GHSER reference state name (V => GHSERVV)"""
