5edfb46f5d38d21438eb1d4955cdfded87afd799
bocklund <brandonbocklund@gmail.com>
2018-04-23 12:44:46 -0400

    FIX: Fix error where SymPy NaNs in calculate_activity_error would raise in emcee

--- espei/error_functions/activity_error.py
+++ espei/error_functions/activity_error.py
@@ -160,4 +160,7 @@ def calculate_activity_error(dbf, comps, phases, datasets, parameters=None, phas
         target_chempots = target_chempots_from_activity(acr_component, np.array(ds['values']).flatten(), conditions[v.T], ref_result)
         # calculate the error
         error += chempot_error(current_chempots, target_chempots)
+    # TODO: write a test for this
+    if np.any(np.isnan(np.array([error], dtype=np.float64))):  # must coerce sympy.core.numbers.Float to float64
+        return -np.inf
     return error
