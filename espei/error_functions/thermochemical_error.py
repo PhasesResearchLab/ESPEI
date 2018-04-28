FIX: Propogate changes in thermochemical error calculation to mcmc module

espei/error_functions/thermochemical_error.py
@@ -149,7 +149,7 @@ def get_prop_samples(dbf, comps, phase_name, desired_data):
     return calculate_dict
 
 
-def calculate_thermochemical_error(dbf, comps, phases, datasets, parameters=None, phase_models=None, callables=None, massfuncs=None):
+def calculate_thermochemical_error(dbf, comps, phases, datasets, parameters=None, phase_models=None, callables=None):
     """
     Calculate the weighted single phase error in the Database
 
@@ -169,9 +169,9 @@ def calculate_thermochemical_error(dbf, comps, phases, datasets, parameters=None
         Phase models to pass to pycalphad calculations. Ideal mixing
         contributions must be removed.
     callables : dict
-        Callables to pass to pycalphad
-    massfuncs : dict
-        Callables to pass to pycalphad
+        Dictionary of {output_property: callables_dict} where callables_dict is
+        a dictionary of {'callables': {phase_name: callables}, 'massfuncs': {phase_name: callables}
+        to pass to pycalphad. These must have ideal mixing portions removed.
 
     Returns
     -------
@@ -214,6 +214,10 @@ def calculate_thermochemical_error(dbf, comps, phases, datasets, parameters=None
     # we could also include the bare property ('' => 'HM'), but these are rarely used in ESPEI
     properties = [''.join(prop) for prop in itertools.product(property_prefix_weight_factor.keys(), property_suffixes)]
 
+    whitelist_properties = ['HM', 'SM', 'CPM', 'HM_MIX', 'SM_MIX', 'CPM_MIX']
+    # if callables is None, construct empty callables dicts, which will be JIT compiled by pycalphad later
+    callables = callables if callables is not None else {prop: {'callables': None, 'massfuncs': None} for prop in whitelist_properties}
+
     sum_square_error = 0
     for phase_name in phases:
         for prop in properties:
@@ -230,8 +234,10 @@ def calculate_thermochemical_error(dbf, comps, phases, datasets, parameters=None
                 calculate_dict['output'] = prop
                 params = parameters
             sample_values = calculate_dict.pop('values')
-            results = calculate(dbf, comps, phase_name, broadcast=False, parameters=params, model=phase_models, massfuncs=massfuncs,
-                                callables=callables, **calculate_dict)[calculate_dict['output']].values
+            results = calculate(dbf, comps, phase_name, broadcast=False, parameters=params, model=phase_models,
+                                massfuncs=callables[calculate_dict['output']]['massfuncs'],
+                                callables=callables[calculate_dict['output']]['callables'],
+                                **calculate_dict)[calculate_dict['output']].values
             weight = (property_prefix_weight_factor[prop.split('_')[0]]*np.abs(np.mean(sample_values)))**(-1.0)
             error = np.sum((results-sample_values)**2) * weight
             # logging.debug('Weighted sum of square error for property {} of phase {}: {}'.format(prop, phase_name, error))

espei/mcmc.py
@@ -23,6 +23,7 @@ def lnprob(params, comps=None, dbf=None, phases=None, datasets=None,
            symbols_to_fit=None, phase_models=None, scheduler=None,
            massfuncs=None, massgradfuncs=None,
            callables=None, grad_callables=None, hess_callables=None,
+           thermochemical_callables=None,
            ):
     """
     Returns the error from multiphase fitting as a log probability.
@@ -39,7 +40,7 @@ def lnprob(params, comps=None, dbf=None, phases=None, datasets=None,
         multi_phase_error = [np.inf]
     multi_phase_error = [np.inf if np.isnan(x) else x ** 2 for x in multi_phase_error]
     multi_phase_error = -np.sum(multi_phase_error)
-    single_phase_error = calculate_thermochemical_error(dbf, comps, phases, datasets, parameters, phase_models=phase_models, callables=callables, massfuncs=massfuncs)
+    single_phase_error = calculate_thermochemical_error(dbf, comps, phases, datasets, parameters, phase_models=phase_models, callables=thermochemical_callables)
     actvity_error = calculate_activity_error(dbf, comps, phases, datasets, parameters=parameters, phase_models=phase_models, callables=callables, grad_callables=grad_callables, hess_callables=hess_callables, massfuncs=massfuncs, massgradfuncs=massgradfuncs)
     total_error = multi_phase_error + single_phase_error + actvity_error
     logging.debug('Single phase error: {:0.2f}. Multi phase error: {:0.2f}. Activity Error: {:0.2f}. Total error: {:0.2f}'.format(single_phase_error, multi_phase_error, actvity_error, total_error))
@@ -147,12 +148,30 @@ def mcmc_fit(dbf, datasets, mcmc_steps=1000, save_interval=100, chains_per_param
     eq_callables = eq_callables_dict(dbf, comps, phases, model=Model, param_symbols=sorted([sympy.Symbol(sym) for sym in symbols_to_fit], key=str))
     # because error_context expencts 'phase_models' key, change it
     eq_callables['phase_models'] = eq_callables.pop('model')
+    # we also need to build models that have no ideal mixing for thermochemical error and to build them for each property we might calculate
+    # TODO: potential optimization to only calculate for phase/property combos that we have in the datasets
+    # first construct dict of models without ideal mixing
+    mods_no_idmix = {}
+    for phase_name in phases:
+        mods_no_idmix[phase_name] = Model(dbf, comps, phase_name)
+        mods_no_idmix[phase_name].models['idmix'] = 0
+    # now construct callables for each possible property that can be calculated
+    thermochemical_callables = {}  # will be dict of {output_property: eq_callables_dict}
+    whitelist_properties = ['HM', 'SM', 'CPM']
+    whitelist_properties = [whitelist_properties] + [prop+'_MIX' for prop in whitelist_properties]
+    for prop in whitelist_properties:
+        thermochemical_callables[prop] = eq_callables_dict(dbf, comps, phases, model=mods_no_idmix, output=prop, param_symbols=sorted([sympy.Symbol(sym) for sym in symbols_to_fit], key=str), build_gradients=False)
+        # pop off the callables not used in properties because we don't want them around (they should be None, anyways)
+        thermochemical_callables[prop].pop('grad_callables')
+        thermochemical_callables[prop].pop('hess_callables')
+        thermochemical_callables[prop].pop('massgradfuncs')
     logging.debug('Finished building phase models')
 
     # context for the log probability function
     error_context = {'comps': comps, 'dbf': dbf,
                      'phases': phases, 'phase_models': eq_callables['phase_models'],
                      'datasets': datasets, 'symbols_to_fit': symbols_to_fit,
+                     'thermochemical_callables': thermochemical_callables,
                      }
 
     error_context.update(**eq_callables)

espei/tests/test_error_functions.py
@@ -26,29 +26,29 @@ def test_activity_error(datasets_db):
 
 def test_thermochemical_error_with_multiple_X_points(datasets_db):
     """Multiple composition datapoints in a dataset for a mixing phase should be successful."""
-    datasets_db.insert(CU_MG_CPM_FORM_X_HCP_A3)
+    datasets_db.insert(CU_MG_CPM_MIX_X_HCP_A3)
 
     dbf = Database(CU_MG_TDB)
-    error = calculate_thermochemical_error(dbf, ['CU','MG','VA'], list(dbf.phases.keys()), datasets_db, {}, {}, {}, {})
+    error = calculate_thermochemical_error(dbf, ['CU','MG','VA'], list(dbf.phases.keys()), datasets_db)
     assert np.isclose(float(error), -520.0, atol=0.01)
 
 
 def test_thermochemical_error_with_multiple_T_points(datasets_db):
     """Multiple temperature datapoints in a dataset for a stoichiometric comnpound should be successful."""
-    datasets_db.insert(CU_MG_HM_FORM_T_CUMG2)
+    datasets_db.insert(CU_MG_HM_MIX_T_CUMG2)
 
     dbf = Database(CU_MG_TDB)
-    error = calculate_thermochemical_error(dbf, ['CU','MG','VA'], list(dbf.phases.keys()), datasets_db, {}, {}, {}, {})
-    assert np.isclose(float(error), -85852620.13414142, atol=0.01)
+    error = calculate_thermochemical_error(dbf, ['CU','MG','VA'], list(dbf.phases.keys()), datasets_db)
+    assert np.isclose(float(error), -3672.727272727273, atol=0.01)
 
 
 def test_thermochemical_error_with_multiple_T_X_points(datasets_db):
     """Multiple temperature and composition datapoints in a dataset for a mixing phase should be successful."""
-    datasets_db.insert(CU_MG_SM_FORM_T_X_FCC_A1)
+    datasets_db.insert(CU_MG_SM_MIX_T_X_FCC_A1)
 
     dbf = Database(CU_MG_TDB)
-    error = calculate_thermochemical_error(dbf, ['CU', 'MG', 'VA'], list(dbf.phases.keys()), datasets_db, {}, {}, {}, {})
-    assert np.isclose(float(error), -31241.312055519393, atol=0.01)
+    error = calculate_thermochemical_error(dbf, ['CU', 'MG', 'VA'], list(dbf.phases.keys()), datasets_db)
+    assert np.isclose(float(error), -31830.303030303032, atol=0.01)
 
 def test_thermochemical_error_for_mixing_entropy_error_is_excess_only(datasets_db):
     """Tests that error in mixing entropy data is excess only (the ideal part is removed)."""