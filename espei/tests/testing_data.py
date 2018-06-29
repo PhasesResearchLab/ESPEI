78e498e39bd9210c15cd8bdaa0e6b80628ab46a6
Brandon Bocklund <brandonbocklund@gmail.com>
2018-06-29 16:19:14 -0400

    TST: Update binary test, add ternary equilibria test

--- espei/tests/testing_data.py
+++ espei/tests/testing_data.py
@@ -244,7 +244,7 @@ CU_MG_DATASET_ZPF_ZERO_ERROR = yaml.load("""{
 """)
 
 
-A_B_DATASET_PHASE_EQUILIBRIA = yaml.load("""{
+A_B_DATASET_BINARY_PHASE_EQUILIBRIA = yaml.load("""{
   "components": ["A", "B"],
   "phases": ["PHASE_1", "PHASE_2", "PHASE_3"],
   "conditions": {
@@ -258,6 +258,25 @@ A_B_DATASET_PHASE_EQUILIBRIA = yaml.load("""{
     [["PHASE_1", ["B"], [0.25]], ["PHASE_2", ["B"], [0.5]], ["PHASE_3", ["B"], [0.75]]]
    ],
   "reference": "testing",
-  "comment": "Examples for 1. one side of tieline, 2. a full tieline, 3. a 3 phase equilibria. For visual checking that plot is correct"
+  "comment": "Examples for 1. one side of tieline, 2. a full tieline, 3. a 3 phase equilibria. For MPL visual checking that plot is correct"
+}
+""")
+
+
+A_B_C_DATASET_TERNARY_PHASE_EQUILIBRIA = yaml.load("""{
+  "components": ["A", "B", "C"],
+  "phases": ["PHASE_1", "PHASE_2", "PHASE_3"],
+  "conditions": {
+    "P": 101325,
+    "T": [300, 300, 300]
+  },
+  "output": "ZPF",
+    "values":   [
+    [["PHASE_1", ["B", "C"], [0.1, 0.1]], ["PHASE_2", ["B", "C"], [null, null]]],
+    [["PHASE_1", ["B", "C"], [0.2, 0.5]], ["PHASE_2", ["B", "C"], [0.3, 0.6]]],
+    [["PHASE_1", ["B", "C"], [0.2, 0.2]], ["PHASE_1", ["B", "C"], [0.3, 0.3]], ["PHASE_2", ["B", "C"], [0.5, 0.1]]],
+   ],
+  "reference": "testing",
+  "comment": "Examples for 1. one side of tieline, 2. a full tieline, 3. a 3 phase equilibria. For MPL visual checking that plot is correct"
 }
 """)
