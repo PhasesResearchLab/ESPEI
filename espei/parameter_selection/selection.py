d43e9963078d92f3af2735a7aa8ade267a9634d8
bocklund <brandonbocklund@gmail.com>
2018-07-12 11:36:42 -0700

    WIP: start refactoring model selection

---
+++ espei/parameter_selection/selection.py
@@ -0,0 +1,94 @@
+"""
+Fit, score and select models
+"""
+
+import logging
+
+import numpy as np
+from sklearn.linear_model import LinearRegression
+
+
+def fit_model(feature_matrix, data_quantities):
+    """
+    Return model coefficients fit by scikit-learn's LinearRegression
+
+    Parameters
+    ----------
+    feature_matrix : ndarray
+        (M*N) regressor matrix. The transformed model inputs (y_i, T, P, etc.)
+    data_quantities : ndarray
+        (M,) response vector. Target values of the output (e.g. HM_MIX) to reproduce.
+
+    Returns
+    -------
+    list
+        List of model coefficients of shape (N,)
+
+    Notes
+    -----
+    Solve Ax = b. `x` are the desired model coefficients. `A` is the
+    'feature_matrix'. `b` corrresponds to 'data_quantities'.
+    """
+    clf = LinearRegression(fit_intercept=False, normalize=True)
+    clf.fit(feature_matrix, data_quantities)
+    return clf.coef_
+
+
+def score_model(feature_matrix, data_quantities, model_coefficients, feature_tuple):
+    """
+    Fit and score a model with the AICc through LinearRegression
+
+    Parameters
+    ----------
+    feature_matrix : ndarray
+        (M*N) regressor matrix. The transformed model inputs (y_i, T, P, etc.)
+    data_quantities : ndarray
+        (M,) response vector. Target values of the output (e.g. HM_MIX) to reproduce.
+    model_coefficients : list
+        List of fitted model coefficients to be scored. Has shape (N,).
+    feature_tuple : list
+        Polynomial coefficients corresponding to each column of 'feature_matrix'.
+        Has shape (N,). Purely a logging aid.
+
+    Returns
+    -------
+    float
+        A model score
+
+    Notes
+    -----
+    Solve Ax = b, where 'feature_matrix' is A and 'data_quantities' is b.
+    """
+    print('Fitting actual parameters in score model')
+    print('Feature matrix')
+    print(feature_matrix)
+    print('Data quantities')
+    print(data_quantities)
+    print('Feature tuple')
+    print(feature_tuple)
+
+    # Now generate candidate models; add parameters one at a time
+    num_params = len(feature_tuple)
+    # This may not exactly be the correct form for the likelihood
+    # We're missing the "ridge" contribution here which could become relevant for sparse data
+    rss = np.square(np.dot(feature_matrix, model_coefficients) - data_quantities.astype(np.float)).sum()
+    # Compute the corrected Akaike Information Criterion
+    # Form valid under assumption all sample variances are random and Gaussian, model is univariate
+    # Our model is univariate with T
+    # The correction is (2k^2 + 2k)/(n - k - 1)
+    # Our denominator for the correction must always be an integer by this equation.
+    # Our correction can blow up if (n-k-1) = 0 and if n - 1 < k (we will actually be *lowering* the AICc)
+    # So we will prevent blowing up by taking the denominator as 1/(p-n+1) for p > n - 1
+    num_samples = data_quantities.size
+    if (num_samples - 1.0) > num_params:
+        correction_denom = num_samples - num_params - 1.0
+    elif (num_samples - 1.0) == num_params:
+        correction_denom = 0.99
+    else:
+        correction_denom = 1.0 / (num_params - num_samples + 1.0)
+    correction = (2.0*num_params**2 + 2.0*num_params)/correction_denom
+    aic = 2.0*num_params + num_samples * np.log(rss/num_samples)
+    aicc = aic + correction  # model score
+    logging.debug('{} rss: {}, AIC: {}, AICc: {}'.format(feature_tuple, rss, aic, aicc))
+    return aicc
+
