8f3a468e4a1025fda61530b6abce6326d036a2fd
bocklund <brandonbocklund@gmail.com>
2019-02-06 10:42:58 -0500

    FIX: Remove non-breaking space unicode character from citation

--- espei/citing.py
+++ espei/citing.py
@@ -2,7 +2,7 @@
 Define citations for ESPEI
 """
 
-ESPEI_CITATION = "﻿B. Bocklund, R. Otis, A. Egorov, A. Obaied, I. Roslyakova, Z.-K. Liu, ESPEI for efficient thermodynamic database development, modification, and uncertainty quantification: application to Cu-Mg, (2019). http://arxiv.org/abs/1902.01269."
+ESPEI_CITATION = "B. Bocklund, R. Otis, A. Egorov, A. Obaied, I. Roslyakova, Z.-K. Liu, ESPEI for efficient thermodynamic database development, modification, and uncertainty quantification: application to Cu-Mg, (2019). http://arxiv.org/abs/1902.01269."
 
 ESPEI_BIBTEX = """@article{Bocklund2019ESPEI,
 abstract = {The software package ESPEI has been developed for efficient evaluation of thermodynamic model parameters within the CALPHAD method. ESPEI uses a linear fitting strategy to parameterize Gibbs energy functions of single phases based on their thermochemical data and refine the model parameters using phase equilibrium data through Bayesian optimization within a Markov Chain Monte Carlo machine learning approach. In this paper, the methodologies employed in ESPEI are discussed in detail and demonstrated for the Cu-Mg system down to 0 K using unary descriptions based on segmented regression. The model parameter uncertainties are quantified and propagated to the Gibbs energy functions.},
