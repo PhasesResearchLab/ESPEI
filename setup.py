54625ca12e01ebf6db9c829e6f5ce40a03f41cf8
bocklund <brandonbocklund@gmail.com>
2019-06-03 14:31:06 +0800

    FIX: Fix setup.py

--- setup.py
+++ setup.py
@@ -62,8 +62,8 @@ setup(
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6'
-        'Programming Language :: Python :: 3.7'
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
     entry_points={'console_scripts': [
                   'espei = espei.espei_script:main']}
