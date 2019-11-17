02946531917dc7285b067df31db42f6c57f39ff8
Jonathan Siegel <jwsiegel@ucla.edu>
2019-11-17 11:11:42 -0500

    Add change to __init__ file to make imports work properly.

--- espei/error_functions/__init__.py
+++ espei/error_functions/__init__.py
@@ -4,5 +4,4 @@ Functions for calculating error.
 
 from .activity_error import calculate_activity_error
 from .thermochemical_error import calculate_thermochemical_error, get_thermochemical_data
-from .zpf_error import get_zpf_data #, calculate_zpf_error
-from .modified_zpf_error import calculate_zpf_error
+from .zpf_error import get_zpf_data, calculate_zpf_error
