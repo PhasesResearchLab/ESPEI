FIX: fixes incorrect per mole-atom/mole-formula data in parameter selection
shift_reference_state now immediately transforms data to J/mole-formula
to be compatible with fixed_models, excluded contributions, and
fixed_portions, which are all in [qty]/mole-atom. New docstring.

espei/parameter_selection/utils.py
@@ -6,24 +6,48 @@ import numpy as np
 import sympy
 from pycalphad import variables as v
 
-feature_transforms = {"CPM_FORM": lambda x: -v.T*sympy.diff(x, v.T, 2),
-                      "CPM_MIX": lambda x: -v.T*sympy.diff(x, v.T, 2),
-                      "CPM": lambda x: -v.T*sympy.diff(x, v.T, 2),
-                      "SM_FORM": lambda x: -sympy.diff(x, v.T),
-                      "SM_MIX": lambda x: -sympy.diff(x, v.T),
-                      "SM": lambda x: -sympy.diff(x, v.T),
-                      "HM_FORM": lambda x: x - v.T*sympy.diff(x, v.T),
-                      "HM_MIX": lambda x: x - v.T*sympy.diff(x, v.T),
-                      "HM": lambda x: x - v.T*sympy.diff(x, v.T)}
+feature_transforms = {"CPM_FORM": lambda GM: -v.T*sympy.diff(GM, v.T, 2),
+                      "CPM_MIX": lambda GM: -v.T*sympy.diff(GM, v.T, 2),
+                      "CPM": lambda GM: -v.T*sympy.diff(GM, v.T, 2),
+                      "SM_FORM": lambda GM: -sympy.diff(GM, v.T),
+                      "SM_MIX": lambda GM: -sympy.diff(GM, v.T),
+                      "SM": lambda GM: -sympy.diff(GM, v.T),
+                      "HM_FORM": lambda GM: GM - v.T*sympy.diff(GM, v.T),
+                      "HM_MIX": lambda GM: GM - v.T*sympy.diff(GM, v.T),
+                      "HM": lambda GM: GM - v.T*sympy.diff(GM, v.T)}
 
 
 def shift_reference_state(desired_data, feature_transform, fixed_model, moles_per_formula_unit):
     """
-    Shift data to a new common reference state.
+    Shift _MIX or _FORM data to a common reference state in per mole-formula units.
+
+    Parameters
+    ----------
+    desired_data : List[Dict[str, Any]]
+        ESPEI single phase dataset
+    feature_transform : Callable
+        Function to transform an AST for the GM property to the property of
+        interest, i.e. entropy would be ``lambda GM: -sympy.diff(GM, v.T)``
+    fixed_model : pycalphad.Model
+        Model with all lower order (in composition) terms already fit.
+    moles_per_formula_unit : float
+        Number of moles of atoms in every mole formula unit.
+
+    Returns
+    -------
+    np.ndarray
+        Data for this feature in [qty]/mole-formula in a common reference state.
+
+    Raises
+    ------
+    ValueError
+
     """
     total_response = []
     for dataset in desired_data:
-        values = np.asarray(dataset['values'], dtype=np.object)
+        # Transform data from J/mole-atom to J/mole-formula (or J/K-mole-atom to J/K-mole-formula etc.)
+        # The pycalphad Model quantities are all per mole-formula
+        values = np.asarray(dataset['values'], dtype=np.object)*moles_per_formula_unit
         if dataset['solver'].get('sublattice_occupancies', None) is not None:
             value_idx = 0
             for occupancy, config in zip(dataset['solver']['sublattice_occupancies'], dataset['solver']['sublattice_configurations']):
@@ -35,9 +59,7 @@ def shift_reference_state(desired_data, feature_transform, fixed_model, moles_pe
                     raise ValueError('Unknown property to shift: {}'.format(dataset['output']))
                 # These contributions are not present in the data, we need to add them here explicitly
                 for excluded_contrib in dataset.get('excluded_model_contributions', []):
-                    values[..., value_idx] += feature_transform(fixed_model.models[excluded_contrib])/moles_per_formula_unit
+                    values[..., value_idx] += feature_transform(fixed_model.models[excluded_contrib])
                 value_idx += 1
         total_response.append(values.flatten())
     return total_response
-
-

espei/paramselect.py
@@ -127,9 +127,9 @@ def fit_formation_energy(dbf, comps, phase_name, configuration, symmetry, datase
     # create the candidate models and fitting steps
     if features is None:
         features = OrderedDict([("CPM_FORM", (v.T * sympy.log(v.T), v.T**2, v.T**-1, v.T**3)),
-                    ("SM_FORM", (v.T,)),
-                    ("HM_FORM", (sympy.S.One,))
-                    ])
+                                ("SM_FORM", (v.T,)),
+                                ("HM_FORM", (sympy.S.One,)),
+                                ])
     # dict of {feature, [candidate_models]}
     candidate_models_features = build_candidate_models(configuration, features)
 
@@ -148,7 +148,7 @@ def fit_formation_energy(dbf, comps, phase_name, configuration, symmetry, datase
     fixed_model = Model(dbf, comps, phase_name, parameters={'GHSER'+(c.upper()*2)[:2]: 0 for c in comps})
     fixed_portions = [0]
 
-    moles_per_formula_unit = sympy.S(0)
+    moles_per_formula_unit = sympy.S(0)  # units: moles-atom/moles-formula-unit
     YS = sympy.Symbol('YS')  # site fraction symbol that we will reuse
     Z = sympy.Symbol('Z')  # site fraction symbol that we will reuse
     V_I, V_J, V_K = sympy.Symbol('V_I'), sympy.Symbol('V_J'), sympy.Symbol('V_K')
@@ -191,11 +191,11 @@ def fit_formation_energy(dbf, comps, phase_name, configuration, symmetry, datase
                 # Remove existing partial model contributions from the data
                 data_qtys = data_qtys - feature_transforms[desired_props[0]](fixed_model.ast)
                 # Subtract out high-order (in T) parameters we've already fit
-                data_qtys = data_qtys - feature_transforms[desired_props[0]](sum(fixed_portions)) / moles_per_formula_unit
+                data_qtys = data_qtys - feature_transforms[desired_props[0]](sum(fixed_portions))
 
                 # if any site fractions show up in our data_qtys that aren't in this datasets site fractions, set them to zero.
                 for sf, i, (_, (sf_product, inter_product)) in zip(site_fractions, data_qtys, all_samples):
-                    missing_variables = sympy.S(i * moles_per_formula_unit).atoms(v.SiteFraction) - set(sf.keys())
+                    missing_variables = sympy.S(i).atoms(v.SiteFraction) - set(sf.keys())
                     sf.update({x: 0. for x in missing_variables})
                     # The equations we have just have the site fractions as YS
                     # and interaction products as Z, so take the product of all
@@ -205,9 +205,8 @@ def fit_formation_energy(dbf, comps, phase_name, configuration, symmetry, datase
                     else:  # Z is probably a number
                         sf.update({YS: sf_product, Z: inter_product})
 
-                # moles_per_formula_unit factor is here because our data is stored per-atom
                 # but all of our fits are per-formula-unit
-                data_qtys = [sympy.S(i * moles_per_formula_unit).xreplace(sf).xreplace({v.T: ixx[0]}).evalf()
+                data_qtys = [sympy.S(i).xreplace(sf).xreplace({v.T: ixx[0]}).evalf()
                                    for i, sf, ixx in zip(data_qtys, site_fractions, all_samples)]
                 data_qtys = np.asarray(data_qtys, dtype=np.float)
                 data_quantities.append(data_qtys)