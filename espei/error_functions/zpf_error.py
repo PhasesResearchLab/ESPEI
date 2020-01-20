722cfbe2e6499c15576d91e8e4d79bfd4f451336
Jonathan Siegel <jwsiegel@ucla.edu>
2020-01-19 20:01:46 -0500

    Fix bug involving hyperplane gradient calculation

--- espei/error_functions/zpf_error.py
+++ espei/error_functions/zpf_error.py
@@ -258,12 +258,14 @@ def calculate_driving_force_at_chem_potential(dbf, chem_pot, species, phase_dict
         if active_phase_count == 0 or (active_phase_count == -1 and data.active):
             optimal_intercept = data.intercept
     for data in hyperplane_data_list:
-        if data.intercept <= optimal_intercept:
+        if data.intercept < optimal_intercept:
             driving_force += optimal_intercept - data.intercept
             grad_vect += data.point_left
-        elif data.active and data.intercept >= optimal_intercept:
+        elif data.active and data.intercept > optimal_intercept:
             driving_force += data.intercept - optimal_intercept
             grad_vect += data.point_right
+        elif data.active and data.intercept == optimal_intercept:
+            grad_vect += 0.5 * (data.point_right + data.point_left)
     output_dict = {'driving_force': driving_force, 'grad_vect': grad_vect}
     return output_dict
 
