PhasesResearchLab/ESPEI

commit c81a7d391d7f35028d97b2b9289e2f3c73c7b3b3

ENH: Use `calculate_` function to compute thermochemical error (#117)
* Breaking changes for the internal API
* Refactor `calculate_thermochemical_error` to `calculate_non_equilibrium_thermochemical_probability`
* `calculate_non_equilibrium_thermochemical_probability` be based on PhaseRecords and use `calculate_` shadow function (performance).
* Don't instantiate `norm` objects when calculating `logpdf`. It's faster and we can broadcast over different standard deviations. Do this for both thermochemical probability and ZPF error.

espei/error_functions/zpf_error.py
@@ -14,15 +14,15 @@ There's some special handling for tieline endpoints where we do not know the
 composition conditions to calculate chemical potentials at.
 """
 
-import operator, logging
-from collections import defaultdict, OrderedDict
+import logging
+from collections import OrderedDict
 from typing import Sequence, Dict, NamedTuple, Any
 
 import numpy as np
 from scipy.stats import norm
 import tinydb
 
-from pycalphad import Database, calculate, equilibrium, Model, variables as v
+from pycalphad import Database, Model, variables as v
 from pycalphad.codegen.callables import build_phase_records
 from pycalphad.core.utils import instantiate_models
 from pycalphad.core.phase_rec import PhaseRecord
@@ -101,6 +101,8 @@ def get_zpf_data(dbf: Database, comps: Sequence[str], phases: Sequence[str], dat
         List of phases to consider
     datasets : espei.utils.PickleableTinyDB
         Datasets that contain single phase data
+    parameters : dict
+        Dictionary mapping symbols to optimize to their initial values
 
     Returns
     -------
@@ -277,8 +279,8 @@ def calculate_zpf_error(zpf_data: Sequence[Dict[str, Any]],
         Datasets that contain single phase data
     phase_models : dict
         Phase models to pass to pycalphad calculations
-    parameters : dict
-        Dictionary of symbols that will be overridden in pycalphad.equilibrium
+    parameters : np.ndarray
+        Array of parameters to calculate the error with.
     callables : dict
         Callables to pass to pycalphad
     data_weight : float
@@ -315,7 +317,7 @@ def calculate_zpf_error(zpf_data: Sequence[Dict[str, Any]],
             eq_str = "conds: ({}), comps: ({})".format(phase_region.potential_conds, ', '.join(['{}: {}'.format(ph, c) for ph, c in zip(phase_region.region_phases, phase_region.comp_conds)]))
             target_hyperplane = estimate_hyperplane(phase_region, parameters, approximate_equilibrium=approximate_equilibrium)
             if np.any(np.isnan(target_hyperplane)):
-                zero_probs = norm(loc=0, scale=1000/data_weight/weight).logpdf(np.zeros(len(region)))
+                zero_probs = norm.logpdf(np.zeros(len(phase_region.comp_conds), loc=0, scale=1000/data_weight/weight))
                 total_zero_prob = np.sum(zero_probs)
                 logging.debug('ZPF error - NaN target hyperplane. Equilibria: ({}), reference: {}. Treating all driving force: 0.0, probability: {}, probabilities: {}'.format(eq_str, dataset_ref, total_zero_prob, zero_probs))
                 prob_error += total_zero_prob
@@ -327,10 +329,9 @@ def calculate_zpf_error(zpf_data: Sequence[Dict[str, Any]],
                                                             phase_region, vertex_idx, parameters,
                                                             approximate_equilibrium=approximate_equilibrium,
                                                             )
-                vertex_prob = norm(loc=0, scale=1000/data_weight/weight).logpdf(driving_force)
+                vertex_prob = norm.logpdf(driving_force, loc=0, scale=1000/data_weight/weight)
                 prob_error += vertex_prob
                 logging.debug('ZPF error - Equilibria: ({}), current phase: {}, driving force: {}, probability: {}, reference: {}'.format(eq_str, phase_region.region_phases[vertex_idx], driving_force, vertex_prob, dataset_ref))
     if np.isnan(prob_error):
         return -np.inf
     return prob_error
-

tests/test_error_functions.py
@@ -8,7 +8,7 @@ from tinydb import where
 from pycalphad import Database
 
 from espei.paramselect import generate_parameters
-from espei.error_functions import calculate_activity_error, calculate_thermochemical_error, calculate_zpf_error, get_thermochemical_data, get_zpf_data
+from espei.error_functions import calculate_activity_error, calculate_non_equilibrium_thermochemical_probability, calculate_zpf_error, get_thermochemical_data, get_zpf_data
 import scipy.stats
 
 from .fixtures import datasets_db
@@ -25,7 +25,7 @@ def test_activity_error(datasets_db):
     assert np.isclose(error, -257.41020886970756, rtol=1e-6)
 
 
-def test_thermochemical_error_with_multiple_X_points(datasets_db):
+def test_non_equilibrium_thermochemical_error_with_multiple_X_points(datasets_db):
     """Multiple composition datapoints in a dataset for a mixing phase should be successful."""
     datasets_db.insert(CU_MG_CPM_MIX_X_HCP_A3)
 
@@ -33,12 +33,12 @@ def test_thermochemical_error_with_multiple_X_points(datasets_db):
     phases = list(dbf.phases.keys())
     comps = ['CU', 'MG', 'VA']
     thermochemical_data = get_thermochemical_data(dbf, comps, phases, datasets_db)
-    error = calculate_thermochemical_error(dbf, comps, thermochemical_data)
+    error = calculate_non_equilibrium_thermochemical_probability(dbf, thermochemical_data)
 
     assert np.isclose(error, -4061.119001241541, rtol=1e-6)
 
 
-def test_thermochemical_error_with_multiple_T_points(datasets_db):
+def test_non_equilibrium_thermochemical_error_with_multiple_T_points(datasets_db):
     """Multiple temperature datapoints in a dataset for a stoichiometric comnpound should be successful."""
     datasets_db.insert(CU_MG_HM_MIX_T_CUMG2)
 
@@ -46,11 +46,11 @@ def test_thermochemical_error_with_multiple_T_points(datasets_db):
     phases = list(dbf.phases.keys())
     comps = ['CU', 'MG', 'VA']
     thermochemical_data = get_thermochemical_data(dbf, comps, phases, datasets_db)
-    error = calculate_thermochemical_error(dbf, comps, thermochemical_data)
+    error = calculate_non_equilibrium_thermochemical_probability(dbf, thermochemical_data)
     assert np.isclose(error,-14.287293263253728, rtol=1e-6)
 
 
-def test_thermochemical_error_with_multiple_T_X_points(datasets_db):
+def test_non_equilibrium_thermochemical_error_with_multiple_T_X_points(datasets_db):
     """Multiple temperature and composition datapoints in a dataset for a mixing phase should be successful."""
     datasets_db.insert(CU_MG_SM_MIX_T_X_FCC_A1)
 
@@ -58,10 +58,10 @@ def test_thermochemical_error_with_multiple_T_X_points(datasets_db):
     phases = list(dbf.phases.keys())
     comps = ['CU', 'MG', 'VA']
     thermochemical_data = get_thermochemical_data(dbf, comps, phases, datasets_db)
-    error = calculate_thermochemical_error(dbf, comps, thermochemical_data)
+    error = calculate_non_equilibrium_thermochemical_probability(dbf, thermochemical_data)
     assert np.isclose(float(error), -3282497.2380024833, rtol=1e-6)
 
-def test_thermochemical_error_for_mixing_entropy_error_is_excess_only(datasets_db):
+def test_non_equilibrium_thermochemical_error_for_mixing_entropy_error_is_excess_only(datasets_db):
     """Tests that error in mixing entropy data is excess only (the ideal part is removed)."""
     # If this fails, make sure the ideal mixing contribution is removed.
     phase_models = {
@@ -109,11 +109,11 @@ def test_thermochemical_error_for_mixing_entropy_error_is_excess_only(datasets_d
     zero_error_prob = scipy.stats.norm(loc=0, scale=0.2).logpdf(0.0)  # SM weight = 0.2
     # Explicitly pass parameters={} to not try fitting anything
     thermochemical_data = get_thermochemical_data(dbf, comps, phases, datasets_db, symbols_to_fit=[])
-    error = calculate_thermochemical_error(dbf, comps, thermochemical_data)
+    error = calculate_non_equilibrium_thermochemical_probability(dbf, thermochemical_data)
     assert np.isclose(error, zero_error_prob, atol=1e-6)
 
 
-def test_thermochemical_error_for_of_enthalpy_mixing(datasets_db):
+def test_non_equilibrium_thermochemical_error_for_of_enthalpy_mixing(datasets_db):
     """Tests that error in mixing enthalpy data is calculated correctly"""
     phase_models = {
         "components": ["AL", "B"],
@@ -160,7 +160,7 @@ def test_thermochemical_error_for_of_enthalpy_mixing(datasets_db):
     zero_error_prob = scipy.stats.norm(loc=0, scale=500.0).logpdf(0.0)  # HM weight = 500
     # Explicitly pass parameters={} to not try fitting anything
     thermochemical_data = get_thermochemical_data(dbf, comps, phases, datasets_db, symbols_to_fit=[])
-    error = calculate_thermochemical_error(dbf, comps, thermochemical_data)
+    error = calculate_non_equilibrium_thermochemical_probability(dbf, thermochemical_data)
     assert np.isclose(error, zero_error_prob, atol=1e-6)
 
 

espei/error_functions/__init__.py
@@ -3,5 +3,5 @@ Functions for calculating error.
 """
 
 from .activity_error import calculate_activity_error
-from .thermochemical_error import calculate_thermochemical_error, get_thermochemical_data
+from .non_equilibrium_thermochemical_error import calculate_non_equilibrium_thermochemical_probability, get_thermochemical_data
 from .zpf_error import calculate_zpf_error, get_zpf_data

espei/error_functions/context.py
@@ -66,20 +66,30 @@ def setup_context(dbf, datasets, symbols_to_fit=None, data_weights=None, make_ca
                             additional_statevars={v.N, v.P, v.T})
     else:
         eq_callables = None
-    thermochemical_data = get_thermochemical_data(dbf, comps, phases, datasets, weight_dict=data_weights, symbols_to_fit=symbols_to_fit, make_callables=make_callables)
     t2 = time.time()
     logging.log(TRACE, 'Finished building phase models ({:0.2f}s)'.format(t2-t1))
+    logging.log(TRACE, 'Getting non-equilibrium thermochemical data (this may take some time)')
+    t1 = time.time()
+    thermochemical_data = get_thermochemical_data(dbf, comps, phases, datasets, weight_dict=data_weights, symbols_to_fit=symbols_to_fit)
+    t2 = time.time()
+    logging.log(TRACE, 'Finished getting non-equilibrium thermochemical data ({:0.2f}s)'.format(t2-t1))
+    logging.log(TRACE, 'Getting ZPF data (this may take some time)')
+    t1 = time.time()
+    zpf_data = get_zpf_data(dbf, comps, phases, datasets, parameters)
+    t2 = time.time()
+    logging.log(TRACE, 'Finished getting ZPF data ({:0.2f}s)'.format(t2-t1))
+
 
     # context for the log probability function
     # for all cases, parameters argument addressed in MCMC loop
     error_context = {
         'symbols_to_fit': symbols_to_fit,
         'zpf_kwargs': {
-            'zpf_data': get_zpf_data(dbf, comps, phases, datasets, parameters),
+            'zpf_data': zpf_data,
             'data_weight': data_weights.get('ZPF', 1.0),
         },
         'thermochemical_kwargs': {
-            'dbf': dbf, 'comps': comps, 'thermochemical_data': thermochemical_data,
+            'dbf': dbf, 'thermochemical_data': thermochemical_data,
         },
         'activity_kwargs': {
             'dbf': dbf, 'comps': comps, 'phases': phases, 'datasets': datasets,

espei/error_functions/non_equilibrium_thermochemical_error.py
@@ -4,20 +4,20 @@ Calculate error due to thermochemical quantities: heat capacity, entropy, enthal
 
 import logging
 from collections import OrderedDict
-from copy import deepcopy
 
 import sympy
 from scipy.stats import norm
 import numpy as np
 from tinydb import where
 
-from pycalphad import calculate, Model, ReferenceState, variables as v
+from pycalphad import Model, ReferenceState, variables as v
 from pycalphad.core.utils import unpack_components, get_pure_elements
-from pycalphad.codegen.callables import build_callables
 
-from espei.refdata import pure_element_phases
 from espei.core_utils import ravel_conditions, get_prop_data
 from espei.utils import database_symbols_to_fit
+from espei.error_functions.zpf_error import update_phase_record_parameters
+from espei.shadow_functions import calculate_
+from pycalphad.codegen.callables import build_phase_records
 
 
 def calculate_points_array(phase_constituents, configuration, occupancies=None):
@@ -130,7 +130,7 @@ def get_prop_samples(dbf, comps, phase_name, desired_data):
     return calculate_dict
 
 
-def get_thermochemical_data(dbf, comps, phases, datasets, weight_dict=None, symbols_to_fit=None, make_callables=True):
+def get_thermochemical_data(dbf, comps, phases, datasets, weight_dict=None, symbols_to_fit=None):
     """
 
     Parameters
@@ -146,7 +146,7 @@ def get_thermochemical_data(dbf, comps, phases, datasets, weight_dict=None, symb
     weight_dict : dict
         Dictionary of weights for each data type, e.g. {'HM': 200, 'SM': 2}
     symbols_to_fit : list
-        Parameters to fit. Used to build the models and callables.
+        Parameters to fit. Used to build the models and PhaseRecords.
 
     Returns
     -------
@@ -168,9 +168,7 @@ def get_thermochemical_data(dbf, comps, phases, datasets, weight_dict=None, symb
         'SM':   0.2/weight_dict.get('SM', 1.0),  # J/K-mol
         'CPM':  0.2/weight_dict.get('CPM', 1.0),  # J/K-mol
     }
-
     properties = ['HM_FORM', 'SM_FORM', 'CPM_FORM', 'HM_MIX', 'SM_MIX', 'CPM_MIX']
-
 
     ref_states = []
     for el in get_pure_elements(dbf, comps):
@@ -188,7 +186,7 @@ def get_thermochemical_data(dbf, comps, phases, datasets, weight_dict=None, symb
                     'phase_name': phase_name,
                     'prop': prop,
                     # needs the following keys to be added:
-                    # calculate_dict, callables, model, output, weights
+                    # species, calculate_dict, phase_records, model, output, weights
                 }
                 # get all the data with these model exclusions
                 if exclusion == tuple([]):
@@ -206,15 +204,18 @@ def get_thermochemical_data(dbf, comps, phases, datasets, weight_dict=None, symb
                 for contrib in exclusion:
                     mod.models[contrib] = sympy.S.Zero
                     mod.reference_model.models[contrib] = sympy.S.Zero
+                species = sorted(map(v.Species, comps))
+                data_dict['species'] = species
                 model = {phase_name: mod}
-                callables = build_callables(dbf, comps, [phase_name], model,
-                                            parameter_symbols=symbols_to_fit,
-                                            output=output,
-                                            build_gradients=False, build_hessians=False,
-                                            additional_statevars={v.P, v.T, v.N})
+                statevar_dict = {getattr(v, c, None): vals for c, vals in calculate_dict.items() if isinstance(getattr(v, c, None), v.StateVariable)}
+                statevar_dict = OrderedDict(sorted(statevar_dict.items(), key=lambda x: str(x[0])))
+                str_statevar_dict = OrderedDict((str(k), vals) for k, vals in statevar_dict.items())
+                phase_records = build_phase_records(dbf, species, [phase_name], statevar_dict, model,
+                                                    output=output, parameters={s: 0 for s in symbols_to_fit},
+                                                    build_gradients=False, build_hessians=False)
+                data_dict['str_statevar_dict'] = str_statevar_dict
+                data_dict['phase_records'] = phase_records
                 data_dict['calculate_dict'] = calculate_dict
-                if make_callables:
-                    data_dict['callables'] = callables
                 data_dict['model'] = model
                 data_dict['output'] = output
                 data_dict['weights'] = np.array(property_std_deviation[prop.split('_')[0]])/np.array(calculate_dict.pop('weights'))
@@ -222,7 +223,7 @@ def get_thermochemical_data(dbf, comps, phases, datasets, weight_dict=None, symb
     return all_data_dicts
 
 
-def calculate_thermochemical_error(dbf, comps, thermochemical_data, parameters=None):
+def calculate_non_equilibrium_thermochemical_probability(dbf, thermochemical_data, parameters=None):
     """
     Calculate the weighted single phase error in the Database
 
@@ -230,12 +231,10 @@ def calculate_thermochemical_error(dbf, comps, thermochemical_data, parameters=N
     ----------
     dbf : pycalphad.Database
         Database to consider
-    comps : list
-        List of active component names
     thermochemical_data : list
         List of thermochemical data dicts
-    parameters : dict
-        Dictionary of symbols that will be overridden in pycalphad.calculate
+    parameters : np.ndarray
+        Array of parameters to calculate the error with.
 
     Returns
     -------
@@ -256,27 +255,23 @@ def calculate_thermochemical_error(dbf, comps, thermochemical_data, parameters=N
 
     """
     if parameters is None:
-        parameters = {}
+        parameters = np.array([])
 
     prob_error = 0.0
     for data in thermochemical_data:
         phase_name = data['phase_name']
-        prop = data['prop']
-        calculate_dict = deepcopy(data['calculate_dict'])
         output = data['output']
-        callables = data.get('callables', None)
-        mod = data['model']
-        std_devs = data['weights']
+        phase_records = data['phase_records']
+        sample_values = data['calculate_dict']['values']
 
-        sample_values = calculate_dict.pop('values')
-        dataset_refs = calculate_dict.pop('references')
-        results = calculate(dbf, comps, phase_name, broadcast=False, parameters=parameters, model=mod,
-                            callables=callables, output=output, **calculate_dict)[output].values
-        probabilities = []
-        differences = []
-        for result, sample_value, std_dev, ref in zip(results, sample_values, std_devs, dataset_refs):
-            differences.append(result-sample_value)
-            probabilities.append(norm(loc=0, scale=std_dev).logpdf(result-sample_value))
-        logging.debug('Thermochemical error - data: {}, differences: {}, probabilities: {}, references: {}'.format(sample_values, differences, probabilities, dataset_refs))
-        prob_error += np.sum(probabilities)
+        update_phase_record_parameters(phase_records, parameters)
+        results = calculate_(dbf, data['species'], [phase_name],
+                             data['str_statevar_dict'], data['model'],
+                             phase_records, output=output, broadcast=False,
+                             points=data['calculate_dict']['points'])[output]
+        differences = results - sample_values
+        probabilities = norm.logpdf(differences, loc=0, scale=data['weights'])
+        prob_sum = np.sum(probabilities)
+        logging.debug(f"Thermochemical error - {data['prop']}({phase_name}) = {prob_sum} - data: {sample_values}, differences: {differences}, probabilities: {probabilities}, references: {data['calculate_dict']['references']}")
+        prob_error += prob_sum
     return prob_error

espei/optimizers/opt_mcmc.py
@@ -3,7 +3,7 @@ import time
 import numpy as np
 import emcee
 from espei.error_functions import calculate_zpf_error, calculate_activity_error, \
-    calculate_thermochemical_error
+    calculate_non_equilibrium_thermochemical_probability
 from espei.priors import PriorSpec, build_prior_specs
 from espei.utils import unpack_piecewise, optimal_parameters
 from espei.error_functions.context import setup_context
@@ -279,7 +279,7 @@ class EmceeOptimizer(OptimizerBase):
         else:
             actvity_error = 0
         if thermochemical_kwargs is not None:
-            single_phase_error = calculate_thermochemical_error(parameters=parameters, **thermochemical_kwargs)
+            single_phase_error = calculate_non_equilibrium_thermochemical_probability(parameters=np.array(params), **thermochemical_kwargs)
         else:
             single_phase_error = 0
         total_error = multi_phase_error + single_phase_error + actvity_error

espei/optimizers/opt_scipy.py
@@ -5,7 +5,7 @@ from scipy.optimize import minimize
 from espei.utils import unpack_piecewise
 from espei.error_functions.context import setup_context
 from espei.error_functions import calculate_activity_error, calculate_zpf_error, \
-    calculate_thermochemical_error
+    calculate_non_equilibrium_thermochemical_probability
 from .opt_base import OptimizerBase
 from .graph import OptNode
 
@@ -51,7 +51,7 @@ class SciPyOptimizer(OptimizerBase):
         else:
             actvity_error = 0
         if thermochemical_kwargs is not None:
-            single_phase_error = calculate_thermochemical_error(parameters=parameters, **thermochemical_kwargs)
+            single_phase_error = calculate_non_equilibrium_thermochemical_probability(parameters=parameters, **thermochemical_kwargs)
         else:
             single_phase_error = 0
         total_error = multi_phase_error + single_phase_error + actvity_error

tests/test_mcmc.py
@@ -50,12 +50,12 @@ def test_lnprob_calculates_single_phase_probability_for_success(datasets_db):
     opt = EmceeOptimizer(dbf)
 
     thermochemical_data = get_thermochemical_data(dbf, comps, phases, datasets_db, symbols_to_fit=[param])
-    thermochemical_kwargs = {'dbf': dbf, 'comps': comps, 'thermochemical_data': thermochemical_data}
+    thermochemical_kwargs = {'dbf': dbf, 'thermochemical_data': thermochemical_data}
     res_orig = opt.predict([orig_val], prior_rvs=[rv_zero()], symbols_to_fit=[param], thermochemical_kwargs=thermochemical_kwargs)
     assert np.isreal(res_orig)
     assert np.isclose(res_orig, -9.119484935312146, rtol=1e-6)
 
-    res_10 = opt.predict([10], prior_rvs=[rv_zero()], symbols_to_fit=[param], thermochemical_kwargs=thermochemical_kwargs)
+    res_10 = opt.predict([10.0], prior_rvs=[rv_zero()], symbols_to_fit=[param], thermochemical_kwargs=thermochemical_kwargs)
     assert np.isreal(res_10)
     assert np.isclose(res_10, -9.143559131626864, rtol=1e-6)
 

commit 4289c08169ab6a04b0f445fd70427b57ad7e7e55

Fix bug where failing equilibrium calculations in ZPF error raised
This bug was introduced by c81a7d3, when it refactored the performance
of calls to logpdf

espei/error_functions/zpf_error.py
@@ -318,7 +318,7 @@ def calculate_zpf_error(zpf_data: Sequence[Dict[str, Any]],
             eq_str = "conds: ({}), comps: ({})".format(phase_region.potential_conds, ', '.join(['{}: {}'.format(ph, c) for ph, c in zip(phase_region.region_phases, phase_region.comp_conds)]))
             target_hyperplane = estimate_hyperplane(phase_region, parameters, approximate_equilibrium=approximate_equilibrium)
             if np.any(np.isnan(target_hyperplane)):
-                zero_probs = norm.logpdf(np.zeros(len(phase_region.comp_conds), loc=0, scale=1000/data_weight/weight))
+                zero_probs = norm.logpdf(np.zeros(len(phase_region.comp_conds)), loc=0, scale=1000/data_weight/weight)
                 total_zero_prob = np.sum(zero_probs)
                 logging.debug('ZPF error - NaN target hyperplane. Equilibria: ({}), reference: {}. Treating all driving force: 0.0, probability: {}, probabilities: {}'.format(eq_str, dataset_ref, total_zero_prob, zero_probs))
                 prob_error += total_zero_prob

tests/test_error_functions.py
@@ -268,3 +268,21 @@ def test_zpf_error_species(datasets_db):
     assert np.isclose(exact_likelihood, -15.6533876)
     approx_likelihood = calculate_zpf_error(zpf_data, approximate_equilibrium=True)
     assert np.isclose(approx_likelihood, -15.6533899)
+
+
+def test_zpf_error_equilibrium_failure(datasets_db):
+    """Test that a hyperplane that fails produce a driving force of zero."""
+    datasets_db.insert(CU_MG_DATASET_ZPF_NAN_EQUILIBRIUM)
+
+    dbf = Database(CU_MG_TDB)
+    comps = ['CU','MG','VA']
+    phases = list(dbf.phases.keys())
+
+    # ZPF weight = 1 kJ and there are two points in the tieline
+    zero_error_probability = 2 * scipy.stats.norm(loc=0, scale=1000.0).logpdf(0.0)
+
+    zpf_data = get_zpf_data(dbf, comps, phases, datasets_db, {})
+    exact_likelihood = calculate_zpf_error(zpf_data)
+    assert np.isclose(exact_likelihood, zero_error_probability, rtol=1e-6)
+    approx_likelihood = calculate_zpf_error(zpf_data)
+    assert np.isclose(approx_likelihood, zero_error_probability, rtol=1e-6)

tests/testing_data.py
@@ -304,6 +304,23 @@ CU_MG_DATASET_ZPF_ZERO_ERROR = yaml.load("""{
 """, Loader=YAML_LOADER)
 
 
+CU_MG_DATASET_ZPF_NAN_EQUILIBRIUM = yaml.load("""{
+  "components": ["CU", "MG", "VA"],
+  "phases": ["FCC_A1", "LAVES_C15"],
+  "conditions": {
+    "P": 101325,
+    "T": 2000
+  },
+  "output": "ZPF",
+    "values":   [
+    [["FCC_A1", ["CU"], [2.0]], ["LIQUID", ["CU"], [null]]]
+    ],
+  "reference": "testing",
+  "comment": "The composition is out of bounds X(CU)=2.0 and should produce an equilibrium failure with NaN GM/NP/MU..."
+}
+""", Loader=YAML_LOADER)
+
+
 CU_MG_DATASET_ZPF_WORKING = yaml.load("""{
     "components": ["CU", "MG", "VA"],
     "phases": ["LIQUID", "FCC_A1"],