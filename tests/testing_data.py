PhasesResearchLab/ESPEI

commit c81a7d391d7f35028d97b2b9289e2f3c73c7b3b3

ENH: Use `calculate_` function to compute thermochemical error (#117)
* Breaking changes for the internal API
* Refactor `calculate_thermochemical_error` to `calculate_non_equilibrium_thermochemical_probability`
* `calculate_non_equilibrium_thermochemical_probability` be based on PhaseRecords and use `calculate_` shadow function (performance).
* Don't instantiate `norm` objects when calculating `logpdf`. It's faster and we can broadcast over different standard deviations. Do this for both thermochemical probability and ZPF error.

espei/error_functions/non_equilibrium_thermochemical_error.py
@@ -4,20 +4,20 @@ Calculate error due to thermochemical quantities: heat capacity, entropy, enthal
 
 import logging
 from collections import OrderedDict
-from copy import deepcopy
 
 import sympy
 from scipy.stats import norm
 import numpy as np
 from tinydb import where
 
-from pycalphad import calculate, Model, ReferenceState, variables as v
+from pycalphad import Model, ReferenceState, variables as v
 from pycalphad.core.utils import unpack_components, get_pure_elements
-from pycalphad.codegen.callables import build_callables
 
-from espei.refdata import pure_element_phases
 from espei.core_utils import ravel_conditions, get_prop_data
 from espei.utils import database_symbols_to_fit
+from espei.error_functions.zpf_error import update_phase_record_parameters
+from espei.shadow_functions import calculate_
+from pycalphad.codegen.callables import build_phase_records
 
 
 def calculate_points_array(phase_constituents, configuration, occupancies=None):
@@ -130,7 +130,7 @@ def get_prop_samples(dbf, comps, phase_name, desired_data):
     return calculate_dict
 
 
-def get_thermochemical_data(dbf, comps, phases, datasets, weight_dict=None, symbols_to_fit=None, make_callables=True):
+def get_thermochemical_data(dbf, comps, phases, datasets, weight_dict=None, symbols_to_fit=None):
     """
 
     Parameters
@@ -146,7 +146,7 @@ def get_thermochemical_data(dbf, comps, phases, datasets, weight_dict=None, symb
     weight_dict : dict
         Dictionary of weights for each data type, e.g. {'HM': 200, 'SM': 2}
     symbols_to_fit : list
-        Parameters to fit. Used to build the models and callables.
+        Parameters to fit. Used to build the models and PhaseRecords.
 
     Returns
     -------
@@ -168,9 +168,7 @@ def get_thermochemical_data(dbf, comps, phases, datasets, weight_dict=None, symb
         'SM':   0.2/weight_dict.get('SM', 1.0),  # J/K-mol
         'CPM':  0.2/weight_dict.get('CPM', 1.0),  # J/K-mol
     }
-
     properties = ['HM_FORM', 'SM_FORM', 'CPM_FORM', 'HM_MIX', 'SM_MIX', 'CPM_MIX']
-
 
     ref_states = []
     for el in get_pure_elements(dbf, comps):
@@ -188,7 +186,7 @@ def get_thermochemical_data(dbf, comps, phases, datasets, weight_dict=None, symb
                     'phase_name': phase_name,
                     'prop': prop,
                     # needs the following keys to be added:
-                    # calculate_dict, callables, model, output, weights
+                    # species, calculate_dict, phase_records, model, output, weights
                 }
                 # get all the data with these model exclusions
                 if exclusion == tuple([]):
@@ -206,15 +204,18 @@ def get_thermochemical_data(dbf, comps, phases, datasets, weight_dict=None, symb
                 for contrib in exclusion:
                     mod.models[contrib] = sympy.S.Zero
                     mod.reference_model.models[contrib] = sympy.S.Zero
+                species = sorted(map(v.Species, comps))
+                data_dict['species'] = species
                 model = {phase_name: mod}
-                callables = build_callables(dbf, comps, [phase_name], model,
-                                            parameter_symbols=symbols_to_fit,
-                                            output=output,
-                                            build_gradients=False, build_hessians=False,
-                                            additional_statevars={v.P, v.T, v.N})
+                statevar_dict = {getattr(v, c, None): vals for c, vals in calculate_dict.items() if isinstance(getattr(v, c, None), v.StateVariable)}
+                statevar_dict = OrderedDict(sorted(statevar_dict.items(), key=lambda x: str(x[0])))
+                str_statevar_dict = OrderedDict((str(k), vals) for k, vals in statevar_dict.items())
+                phase_records = build_phase_records(dbf, species, [phase_name], statevar_dict, model,
+                                                    output=output, parameters={s: 0 for s in symbols_to_fit},
+                                                    build_gradients=False, build_hessians=False)
+                data_dict['str_statevar_dict'] = str_statevar_dict
+                data_dict['phase_records'] = phase_records
                 data_dict['calculate_dict'] = calculate_dict
-                if make_callables:
-                    data_dict['callables'] = callables
                 data_dict['model'] = model
                 data_dict['output'] = output
                 data_dict['weights'] = np.array(property_std_deviation[prop.split('_')[0]])/np.array(calculate_dict.pop('weights'))
@@ -222,7 +223,7 @@ def get_thermochemical_data(dbf, comps, phases, datasets, weight_dict=None, symb
     return all_data_dicts
 
 
-def calculate_thermochemical_error(dbf, comps, thermochemical_data, parameters=None):
+def calculate_non_equilibrium_thermochemical_probability(dbf, thermochemical_data, parameters=None):
     """
     Calculate the weighted single phase error in the Database
 
@@ -230,12 +231,10 @@ def calculate_thermochemical_error(dbf, comps, thermochemical_data, parameters=N
     ----------
     dbf : pycalphad.Database
         Database to consider
-    comps : list
-        List of active component names
     thermochemical_data : list
         List of thermochemical data dicts
-    parameters : dict
-        Dictionary of symbols that will be overridden in pycalphad.calculate
+    parameters : np.ndarray
+        Array of parameters to calculate the error with.
 
     Returns
     -------
@@ -256,27 +255,23 @@ def calculate_thermochemical_error(dbf, comps, thermochemical_data, parameters=N
 
     """
     if parameters is None:
-        parameters = {}
+        parameters = np.array([])
 
     prob_error = 0.0
     for data in thermochemical_data:
         phase_name = data['phase_name']
-        prop = data['prop']
-        calculate_dict = deepcopy(data['calculate_dict'])
         output = data['output']
-        callables = data.get('callables', None)
-        mod = data['model']
-        std_devs = data['weights']
+        phase_records = data['phase_records']
+        sample_values = data['calculate_dict']['values']
 
-        sample_values = calculate_dict.pop('values')
-        dataset_refs = calculate_dict.pop('references')
-        results = calculate(dbf, comps, phase_name, broadcast=False, parameters=parameters, model=mod,
-                            callables=callables, output=output, **calculate_dict)[output].values
-        probabilities = []
-        differences = []
-        for result, sample_value, std_dev, ref in zip(results, sample_values, std_devs, dataset_refs):
-            differences.append(result-sample_value)
-            probabilities.append(norm(loc=0, scale=std_dev).logpdf(result-sample_value))
-        logging.debug('Thermochemical error - data: {}, differences: {}, probabilities: {}, references: {}'.format(sample_values, differences, probabilities, dataset_refs))
-        prob_error += np.sum(probabilities)
+        update_phase_record_parameters(phase_records, parameters)
+        results = calculate_(dbf, data['species'], [phase_name],
+                             data['str_statevar_dict'], data['model'],
+                             phase_records, output=output, broadcast=False,
+                             points=data['calculate_dict']['points'])[output]
+        differences = results - sample_values
+        probabilities = norm.logpdf(differences, loc=0, scale=data['weights'])
+        prob_sum = np.sum(probabilities)
+        logging.debug(f"Thermochemical error - {data['prop']}({phase_name}) = {prob_sum} - data: {sample_values}, differences: {differences}, probabilities: {probabilities}, references: {data['calculate_dict']['references']}")
+        prob_error += prob_sum
     return prob_error

espei/error_functions/zpf_error.py
@@ -14,15 +14,15 @@ There's some special handling for tieline endpoints where we do not know the
 composition conditions to calculate chemical potentials at.
 """
 
-import operator, logging
-from collections import defaultdict, OrderedDict
+import logging
+from collections import OrderedDict
 from typing import Sequence, Dict, NamedTuple, Any
 
 import numpy as np
 from scipy.stats import norm
 import tinydb
 
-from pycalphad import Database, calculate, equilibrium, Model, variables as v
+from pycalphad import Database, Model, variables as v
 from pycalphad.codegen.callables import build_phase_records
 from pycalphad.core.utils import instantiate_models
 from pycalphad.core.phase_rec import PhaseRecord
@@ -101,6 +101,8 @@ def get_zpf_data(dbf: Database, comps: Sequence[str], phases: Sequence[str], dat
         List of phases to consider
     datasets : espei.utils.PickleableTinyDB
         Datasets that contain single phase data
+    parameters : dict
+        Dictionary mapping symbols to optimize to their initial values
 
     Returns
     -------
@@ -277,8 +279,8 @@ def calculate_zpf_error(zpf_data: Sequence[Dict[str, Any]],
         Datasets that contain single phase data
     phase_models : dict
         Phase models to pass to pycalphad calculations
-    parameters : dict
-        Dictionary of symbols that will be overridden in pycalphad.equilibrium
+    parameters : np.ndarray
+        Array of parameters to calculate the error with.
     callables : dict
         Callables to pass to pycalphad
     data_weight : float
@@ -315,7 +317,7 @@ def calculate_zpf_error(zpf_data: Sequence[Dict[str, Any]],
             eq_str = "conds: ({}), comps: ({})".format(phase_region.potential_conds, ', '.join(['{}: {}'.format(ph, c) for ph, c in zip(phase_region.region_phases, phase_region.comp_conds)]))
             target_hyperplane = estimate_hyperplane(phase_region, parameters, approximate_equilibrium=approximate_equilibrium)
             if np.any(np.isnan(target_hyperplane)):
-                zero_probs = norm(loc=0, scale=1000/data_weight/weight).logpdf(np.zeros(len(region)))
+                zero_probs = norm.logpdf(np.zeros(len(phase_region.comp_conds), loc=0, scale=1000/data_weight/weight))
                 total_zero_prob = np.sum(zero_probs)
                 logging.debug('ZPF error - NaN target hyperplane. Equilibria: ({}), reference: {}. Treating all driving force: 0.0, probability: {}, probabilities: {}'.format(eq_str, dataset_ref, total_zero_prob, zero_probs))
                 prob_error += total_zero_prob
@@ -327,10 +329,9 @@ def calculate_zpf_error(zpf_data: Sequence[Dict[str, Any]],
                                                             phase_region, vertex_idx, parameters,
                                                             approximate_equilibrium=approximate_equilibrium,
                                                             )
-                vertex_prob = norm(loc=0, scale=1000/data_weight/weight).logpdf(driving_force)
+                vertex_prob = norm.logpdf(driving_force, loc=0, scale=1000/data_weight/weight)
                 prob_error += vertex_prob
                 logging.debug('ZPF error - Equilibria: ({}), current phase: {}, driving force: {}, probability: {}, reference: {}'.format(eq_str, phase_region.region_phases[vertex_idx], driving_force, vertex_prob, dataset_ref))
     if np.isnan(prob_error):
         return -np.inf
     return prob_error
-

commit 5ed752450ad7e368bd3e0a02e434b4432cfcee83

FIX: Harden ZPF error, fix bug in failing equilibrium calls (#120)
* Use unpack_components for getting data, rather than mapping v.Species

Mapping v.Species could be dangerous since the components likely do not
include SPECIES and only pure elements. We should use unpack_components

It appears that we don't *need* it, almost out of luck, because all the
current consumers of species use unpack_components internally in the
important places, but this is updated no less.

* Test that ZPF error works for a species

* Fix bug where failing equilibrium calculations in ZPF error raised

This bug was introduced by c81a7d3, when it refactored the performance
of calls to logpdf

* Dead code

* Test that asserts stoichiometric compounds work

* Copy edit docs: canceLLed to canceLed because America

espei/error_functions/non_equilibrium_thermochemical_error.py
@@ -204,7 +204,7 @@ def get_thermochemical_data(dbf, comps, phases, datasets, weight_dict=None, symb
                 for contrib in exclusion:
                     mod.models[contrib] = sympy.S.Zero
                     mod.reference_model.models[contrib] = sympy.S.Zero
-                species = sorted(map(v.Species, comps))
+                species = sorted(unpack_components(dbf, comps), key=str)
                 data_dict['species'] = species
                 model = {phase_name: mod}
                 statevar_dict = {getattr(v, c, None): vals for c, vals in calculate_dict.items() if isinstance(getattr(v, c, None), v.StateVariable)}

espei/error_functions/zpf_error.py
@@ -24,7 +24,7 @@ import tinydb
 
 from pycalphad import Database, Model, variables as v
 from pycalphad.codegen.callables import build_phase_records
-from pycalphad.core.utils import instantiate_models, filter_phases
+from pycalphad.core.utils import instantiate_models, filter_phases, unpack_components
 from pycalphad.core.phase_rec import PhaseRecord
 from espei.utils import PickleableTinyDB
 from espei.shadow_functions import equilibrium_, calculate_, no_op_equilibrium_
@@ -118,7 +118,7 @@ def get_zpf_data(dbf: Database, comps: Sequence[str], phases: Sequence[str], dat
     zpf_data = []  # 1:1 correspondence with each dataset
     for data in desired_data:
         data_comps = list(set(data['components']).union({'VA'}))
-        species = sorted(map(v.Species, data_comps), key=str)
+        species = sorted(unpack_components(dbf, data_comps), key=str)
         data_phases = filter_phases(dbf, species, candidate_phases=phases)
         models = instantiate_models(dbf, species, data_phases, parameters=parameters)
         all_regions = data['values']
@@ -318,13 +318,12 @@ def calculate_zpf_error(zpf_data: Sequence[Dict[str, Any]],
             eq_str = "conds: ({}), comps: ({})".format(phase_region.potential_conds, ', '.join(['{}: {}'.format(ph, c) for ph, c in zip(phase_region.region_phases, phase_region.comp_conds)]))
             target_hyperplane = estimate_hyperplane(phase_region, parameters, approximate_equilibrium=approximate_equilibrium)
             if np.any(np.isnan(target_hyperplane)):
-                zero_probs = norm.logpdf(np.zeros(len(phase_region.comp_conds), loc=0, scale=1000/data_weight/weight))
+                zero_probs = norm.logpdf(np.zeros(len(phase_region.comp_conds)), loc=0, scale=1000/data_weight/weight)
                 total_zero_prob = np.sum(zero_probs)
                 logging.debug('ZPF error - NaN target hyperplane. Equilibria: ({}), reference: {}. Treating all driving force: 0.0, probability: {}, probabilities: {}'.format(eq_str, dataset_ref, total_zero_prob, zero_probs))
                 prob_error += total_zero_prob
                 continue
             # Then calculate the driving force to that hyperplane for each individual vertex
-            #     # region_phases, comp_conds, phase_flags, phase_records
             for vertex_idx in range(len(phase_region.comp_conds)):
                 driving_force = driving_force_to_hyperplane(target_hyperplane, data_comps,
                                                             phase_region, vertex_idx, parameters,

tests/test_error_functions.py
@@ -247,3 +247,63 @@ def test_subsystem_zpf_probability(datasets_db):
     zpf_data = get_zpf_data(dbf_tern, ['CR', 'FE', 'NI', 'VA'], phases, datasets_db, {})
     prob = calculate_zpf_error(zpf_data, np.array([]))
     assert np.isclose(prob, bin_prob)
+
+
+def test_zpf_error_species(datasets_db):
+    """Tests that ZPF error works if a species is used."""
+
+    # Note that the liquid is stabilized by the species for the equilibrium
+    # used in the data. If the SPECIES is removed from the database (and LIQUID
+    # constituents), then the resulting likelihood will NOT match this (and be
+    # closer to 93, according to a test.)
+
+    datasets_db.insert(LI_SN_ZPF_DATA)
+
+    dbf = Database(LI_SN_TDB)
+    comps = ['LI', 'SN']
+    phases = list(dbf.phases.keys())
+
+    # ZPF weight = 1 kJ and there are two points in the tieline
+    zero_error_probability = 2 * scipy.stats.norm(loc=0, scale=1000.0).logpdf(0.0)
+
+    zpf_data = get_zpf_data(dbf, comps, phases, datasets_db, {})
+    exact_likelihood = calculate_zpf_error(zpf_data, approximate_equilibrium=False)
+    assert np.isclose(exact_likelihood, zero_error_probability)
+    approx_likelihood = calculate_zpf_error(zpf_data, approximate_equilibrium=True)
+    assert np.isclose(approx_likelihood, zero_error_probability)
+
+
+def test_zpf_error_equilibrium_failure(datasets_db):
+    """Test that a hyperplane that fails produce a driving force of zero."""
+    datasets_db.insert(CU_MG_DATASET_ZPF_NAN_EQUILIBRIUM)
+
+    dbf = Database(CU_MG_TDB)
+    comps = ['CU','MG','VA']
+    phases = list(dbf.phases.keys())
+
+    # ZPF weight = 1 kJ and there are two points in the tieline
+    zero_error_probability = 2 * scipy.stats.norm(loc=0, scale=1000.0).logpdf(0.0)
+
+    zpf_data = get_zpf_data(dbf, comps, phases, datasets_db, {})
+    exact_likelihood = calculate_zpf_error(zpf_data)
+    assert np.isclose(exact_likelihood, zero_error_probability, rtol=1e-6)
+    approx_likelihood = calculate_zpf_error(zpf_data)
+    assert np.isclose(approx_likelihood, zero_error_probability, rtol=1e-6)
+
+
+def test_zpf_error_works_for_stoichiometric_cmpd_tielines(datasets_db):
+    """A stochimetric compound with approximate composition can be in the datasets and work"""
+    datasets_db.insert(CU_MG_DATASET_ZPF_STOICH_COMPOUND)
+
+    dbf = Database(CU_MG_TDB)
+    comps = ['CU','MG']
+    phases = list(dbf.phases.keys())
+
+    # ZPF weight = 1 kJ and there are two points in the tieline
+    zero_error_probability = 2 * scipy.stats.norm(loc=0, scale=1000.0).logpdf(0.0)
+
+    zpf_data = get_zpf_data(dbf, comps, phases, datasets_db, {})
+    exact_likelihood = calculate_zpf_error(zpf_data)
+    assert np.isclose(exact_likelihood, zero_error_probability, rtol=1e-6)
+    approx_likelihood = calculate_zpf_error(zpf_data)
+    assert np.isclose(approx_likelihood, zero_error_probability, rtol=1e-6)

tests/testing_data.py
@@ -304,6 +304,40 @@ CU_MG_DATASET_ZPF_ZERO_ERROR = yaml.load("""{
 """, Loader=YAML_LOADER)
 
 
+CU_MG_DATASET_ZPF_STOICH_COMPOUND = yaml.load("""{
+  "components": ["CU", "MG"],
+  "phases": ["LIQUID", "CUMG2"],
+  "conditions": {
+    "P": 101325,
+    "T": 400
+  },
+  "output": "ZPF",
+    "values":   [
+    [["CUMG2", ["CU"], [0.3333333333]], ["LIQUID", ["CU"], [null]]]
+    ],
+  "reference": "testing",
+  "comment": "A stoichiometric compound is stable and the composition is approximate."
+}
+""", Loader=YAML_LOADER)
+
+
+CU_MG_DATASET_ZPF_NAN_EQUILIBRIUM = yaml.load("""{
+  "components": ["CU", "MG", "VA"],
+  "phases": ["FCC_A1", "LIQUID"],
+  "conditions": {
+    "P": 101325,
+    "T": 2000
+  },
+  "output": "ZPF",
+    "values":   [
+    [["FCC_A1", ["CU"], [2.0]], ["LIQUID", ["CU"], [null]]]
+    ],
+  "reference": "testing",
+  "comment": "The composition is out of bounds X(CU)=2.0 and should produce an equilibrium failure with NaN GM/NP/MU..."
+}
+""", Loader=YAML_LOADER)
+
+
 CU_MG_DATASET_ZPF_WORKING = yaml.load("""{
     "components": ["CU", "MG", "VA"],
     "phases": ["LIQUID", "FCC_A1"],
@@ -868,3 +902,178 @@ Constituent FCC_A1 : CR,NI : VA : !
 
 """
 
+LI_SN_TDB = """$ Li-Sn
+$--------1---------2---------3---------4---------5---------6---------7---------8
+$
+$ TDB-file for the thermodynamic assessment of the Li-Sn system
+$
+$-----------------------------------------------------------------------------
+$ 2015.1.15
+$
+$ TDB file created by T.Abe and S.Fujita
+$
+$ Thermodynamics Modeling Group, National Institute for
+$ Materials Science. 1-2-1 Sengen, Tsukuba, Ibaraki 305-0047, Japan
+$ e-mail: abe.taichi (at) nims.go.jp
+$ Copyright (C) NIMS 2015
+$
+$ ------------------------------------------------------------------------------
+$ PARAMETERS ARE TAKEN FROM
+$
+$ Thermodynamic assessment and experimental investigation of the Li-Sn system.
+$ D. Li, S. Furtauer, H. Flandorfer, D.M. Cupid
+$
+$ CALPHAD: Computer Coupling of Phase Diagrams and Thermochemistry
+$ 47 (2014) 181-195
+$ ------------------------------------------------------------------------------
+ ELEMENT /-   ELECTRON_GAS              0.0000E+00  0.0000E+00  0.0000E+00!
+ ELEMENT VA   VACUUM                    0.0000E+00  0.0000E+00  0.0000E+00!
+ ELEMENT LI   BCC_A2                    6.9410E+00  4.6233E+03  2.9095E+01 !
+ ELEMENT SN   BCT_A5                    1.1871E+02  6.3220E+03  5.1195E+01 !
+
+ SPECIES LI4SN                          LI4SN1 !
+$ ------------------------------------------------------------------------------
+ TYPE_DEFINITION % SEQ *!
+ DEFINE_SYSTEM_DEFAULT ELEMENT 2 !
+ DEFAULT_COMMAND DEF_SYS_ELEMENT VA /- !
+
+$ ------------------------------------------------------------------------------
+$ Li
+$ ------------------------------------------------------------------------------
+$ Unary 5.1
+ FUNCTION GHSERLI  200.00
+ -10583.817+217.637482*T-38.940488*T*LN(T)+35.466931E-3*T**2-19.869816E-6*T**3
+ +159994*T**(-1);   453.60 Y
+ -559579.123+10547.879893*T-1702.8886493*T*LN(T)+2258.329444E-3*T**2
+ -571.066077E-6*T**3+33885874*T**(-1);   500.00 Y
+ -9062.994+179.278285*T-31.2283718*T*LN(T)+2.633221E-3*T**2-0.438058E-6*T**3
+ -102387*T**(-1);  3000.00 N !
+
+ FUNCTION GLIQLI   200.00
+ -7883.612+211.841861*T-38.940488*T*LN(T)+35.466931E-3*T**2-19.869816E-6*T**3
+ +159994*T**(-1);   250.00 Y
+ 12015.027-362.187078*T+61.6104424*T*LN(T)-182.426463E-3*T**2+63.955671E-6*T**3
+ -559968*T**(-1);   453.60 Y
+ -6057.31+172.652183*T-31.2283718*T*LN(T)+2.633221E-3*T**2-0.438058E-6*T**3
+ -102387*T**(-1);  3000.00 N !
+
+ FUNCTION GFCCLI 200 -108+1.3*T+GHSERLI; 3000 N !
+
+ FUNCTION GHCPLI 200 -154+2*T+GHSERLI; 3000 N !
+$ ------------------------------------------------------------------------------
+$ Sn
+$ ------------------------------------------------------------------------------
+$ Unary 5.1
+ FUNCTION GHSERSN  100.00
+ -7958.517+122.765451*T-25.858*T*LN(T)+0.51185E-3*T**2-3.192767E-6*T**3
+ +18440*T**(-1);   250.00 Y
+ -5855.135+65.443315*T-15.961*T*LN(T)-18.8702E-3*T**2+3.121167E-6*T**3
+ -61960*T**(-1);   505.078 Y
+ 2524.724+4.005269*T-8.2590486*T*LN(T)-16.814429E-3*T**2+2.623131E-6*T**3
+ -1081244*T**(-1)-123.07E23*T**(-9);   800.00 Y
+ -8256.959+138.99688*T-28.4512*T*LN(T)-123.07E23*T**(-9);  3000.00 N !
+
+ FUNCTION GLIQSN 100 7103.092-14.087767*T+147.031E-20*T**7+GHSERSN; 505.078 Y
+ 9496.31-9.809114*T-8.2590486*T*LN(T)-16.814429E-3*T**2+2.623131E-6*T**3
+ -1081244*T**(-1);   800.00 Y
+ -1285.372+125.182498*T-28.4512*T*LN(T);  3000.00 N !
+
+ FUNCTION GFCCSN 100 5510-8.46*T+GHSERSN; 3000 N !
+
+ FUNCTION GHCPSN 100 3900-7.646*T+GHSERSN; 3000 N !
+
+ FUNCTION GBCCSN 100 4400-6*T+GHSERSN; 3000 N !
+
+ FUNCTION GDIASN 0  100
+ -9579.608+114.007785*T-22.972*T*LN(T)-8.13975E-3*T**2+2.7288E-6*T**3
+ +25615*T**(-1);   298.15 Y
+ -9063.001+104.84654*T-21.5750771*T*LN(T)-8.575282E-3*T**2+1.784447E-6*T**3
+ -2544*T**(-1);   800.00 Y
+ -10909.353+147.396537*T-28.4512*T*LN(T);  3000.00 N !
+$ ------------------------------------------------------------------------------
+$ PARAMETERS FOR LIQUID PHASE
+$ ------------------------------------------------------------------------------
+ PHASE LIQUID % 1 1 !
+ CONSTITUENT LIQUID : LI, LI4SN, SN : !
+ PARAMETER G(LIQUID,LI;0) 298.15 +GLIQLI;                              3000 N !
+ PARAMETER G(LIQUID,SN;0) 298.15 +GLIQSN;                              3000 N !
+ PARAMETER G(LIQUID,LI4SN;0) 298.15 -205264.343+187.457195*T-16.7055876*T*LN(T)
+ +4*GLIQLI+GLIQSN;                                                      3000 N !
+ PARAMETER G(LIQUID,LI,SN;0) 298.15 -64124.4038-15.9626763*T
+ +2.04164413*T*LN(T) ;                                                 3000 N !
+ PARAMETER G(LIQUID,LI,LI4SN,SN;0) 298.15 30074.1641-19.0031351*T
+ -1.46094182*T*LN(T) ;                                                 3000 N !
+ PARAMETER G(LIQUID,LI4SN,SN;0) 298.15 -24652.1452-259.57801*T
+ +28.4690441*T*LN(T) ;                                                 3000 N !
+$ ------------------------------------------------------------------------------
+$ PARAMETERS FOR BCC PHASE
+$ ------------------------------------------------------------------------------
+ PHASE BCC % 1 1 !
+ CONSTITUENT BCC : LI, SN : !
+ PARAMETER G(BCC,LI;0) 298.15 +GHSERLI;                                3000 N !
+ PARAMETER G(BCC,SN;0) 298.15 +GBCCSN;                                 3000 N !
+$ ------------------------------------------------------------------------------
+$ PARAMETERS FOR BCT PHASE
+$ ------------------------------------------------------------------------------
+ PHASE BCT % 1 1 !
+ CONSTITUENT BCT : SN : !
+ PARAMETER G(BCT,SN;0) 298.15 +GHSERSN;                                3000 N !
+$ ------------------------------------------------------------------------------
+$ PARAMETERS FOR DIAMOND PHASE
+$ ------------------------------------------------------------------------------
+ PHASE DIAMOND % 1 1 !
+ CONSTITUENT DIAMOND : SN : !
+ PARAMETER G(DIAMOND,SN;0) 298.15 +GDIASN;                         3000 N !
+$ ------------------------------------------------------------------------------
+$ PARAMETERS FOR STOICHIOMETRIC PHASE
+$ ------------------------------------------------------------------------------
+ PHASE LI17SN4 % 2 0.81 0.19 !
+ CONSTITUENT LI17SN4 : LI : SN : !
+ PARAMETER G(LI17SN4,LI:SN;0) 298.15
+ -47307.6838+168.904051*T-29*T*LN(T);                              3000 N !
+
+ PHASE LI7SN2 % 2 0.778 0.222 !
+ CONSTITUENT LI7SN2 : LI,SN : SN : !
+ PARAMETER G(LI7SN2,LI:SN;0) 298.15 -48940+167.201983*T-29*T*LN(T); 3000 N !
+ PARAMETER G(LI7SN2,SN:SN;0) 298.15 +5000;                  3000 N !
+ PARAMETER G(LI7SN2,LI,SN:SN;0) 298.15 0;                           3000 N !
+ PARAMETER G(LI7SN2,LI,SN:SN;1) 298.15 -130*T;                      3000 N !
+
+ PHASE LI13SN5 % 2 0.722 0.278 !
+ CONSTITUENT LI13SN5 : LI : SN : !
+ PARAMETER G(LI13SN5,LI:SN;0) 298.15 -51045+166.043371*T-29*T*LN(T); 3000 N !
+
+ PHASE LI5SN2 % 2 0.714 0.286 !
+ CONSTITUENT LI5SN2 : LI : SN : !
+ PARAMETER G(LI5SN2,LI:SN;0) 298.15 -50890+165.654838*T-29*T*LN(T);  3000 N !
+
+ PHASE LI7SN3 % 2 0.7 0.3 !
+ CONSTITUENT LI7SN3 : LI : SN : !
+ PARAMETER G(LI7SN3,LI:SN;0) 298.15 -50500+165.335224*T-29*T*LN(T); 3000 N !
+
+ PHASE LISN % 2 0.5 0.5 !
+ CONSTITUENT LISN : LI : SN : !
+ PARAMETER G(LISN,LI:SN;0) 298.15 -41200+156.993388*T-29*T*LN(T);  3000 N !
+
+ PHASE LI2SN5 % 2 0.286 0.714 !
+ CONSTITUENT LI2SN5 : LI : SN : !
+ PARAMETER G(LI2SN5,LI:SN;0) 298.15 -33000+158.281465*T-29*T*LN(T); 3000 N !
+$ ----------------------------------------------------------------- END OF LINE
+$
+"""
+
+
+LI_SN_ZPF_DATA = {
+    "components": ["LI", "SN"],
+    "phases": ["LIQUID", "LI7SN2"],
+    "broadcast_conditions": False,
+    "conditions": {
+        "T": [1040],
+        "P": [101325.0]
+    },
+    "output": "ZPF",
+    "values": [
+        [["LIQUID", ["SN"], [0.25864665]], ["LI7SN2", ["SN"], [0.24402028]]]
+    ],
+    "reference": "zpf test", "comment": "Exact tieline based on equilibrium calculation"
+}