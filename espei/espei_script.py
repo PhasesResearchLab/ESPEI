b4bffa129d51f5021c39f8c0440e2cfb42e74fe8
bocklund <brandonbocklund@gmail.com>
2020-11-12 13:17:29 -0500

    Remove extra get_dask_config_paths definition

--- espei/espei_script.py
+++ espei/espei_script.py
@@ -62,20 +62,6 @@ def log_version_info():
     logging.debug('emcee version       ' + str(emcee.__version__))
     logging.info("If you use ESPEI for work presented in a publication, we ask that you cite the following paper:\n    {}".format(espei.__citation__))
 
-def get_dask_config_paths():
-    candidates = dask.config.paths
-    file_paths = []
-    for path in candidates:
-        if os.path.exists(path):
-            if os.path.isdir(path):
-                file_paths.extend(sorted([
-                    os.path.join(path, p)
-                    for p in os.listdir(path)
-                    if os.path.splitext(p)[1].lower() in ('.json', '.yaml', '.yml')
-                ]))
-            else:
-                file_paths.append(path)
-    return file_paths
 
 def _raise_dask_work_stealing():
     """
