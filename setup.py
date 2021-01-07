782870ca3aa1ea0fa7ce0dfc8fcce24037327f57
bocklund <brandonbocklund@gmail.com>
2021-01-07 17:25:26 -0500

    Add Python 3.9 to setup.py support and to the test matrix

--- setup.py
+++ setup.py
@@ -64,6 +64,7 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
     entry_points={'console_scripts': [
                   'espei = espei.espei_script:main']}
