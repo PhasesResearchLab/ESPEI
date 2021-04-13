PhasesResearchLab/ESPEI

commit 78a812a07dcc95ffdacc2ad07704a31773f87b15

ENH/MAINT: Refactor database initialization and enable custom SER reference data (#158)
This PR:

* Extracts a public `initialize_database` function from `generate_parameters` to bootstrap database from phase models and reference state data
* Changes the API for `phase_fit` to remove unnecessary phase data parameters that are in the database already and to update the API to use global aliases (for now, the aliases still use the phase-local versions internally, but this enables later changes to improve how aliases are used to find data without changing the API further).
* Enables providing SER reference data (phase names, atomic mass, H298 and S298 data) to override the SGTE data (fully supports fictitious elements). This includes updated documentation on providing user reference states to add information about the SER dict. Adding the SER dict is currently optional and will fall back to the SGTE data. This is both backwards compatible and a useful default, since most usages of custom unary reference data will probably only want to update the Gibbs energy functions, not the SER data.
* (0, True) is now added explicitly and hardcoded into the SGTE91 and SR2016 unary expressions instead of added on the fly. The additions were modifying the original data, so subsequent ESPEI runs in the same session continually appended `(0, True)` entries. Appending `(0, True)` was safe, even if users provided a similar safeguard themselves, but explicit is better than implicit. This also helps to clarify how pycalphad extrapolates and encourages users to think carefully about extrapolation of custom reference state data.

Closes #150 for now. The official workarounds are to either add these as phases to the phase models (perhaps through aliasing a `GAS` phase) or by using a custom SER reference state.

(Note the size of the changes is a little misleading because of all the reference data updated. In actual ESPEI modules (not tests) only 167 additions and 60 deletions.)

espei/database_utils.py
@@ -1,0 +1,113 @@
+"""Provides utilities for creating and working with Databases in ESPEI
+"""
+
+import logging
+from typing import Dict, Union
+import sympy
+from pycalphad import Database, variables as v
+import espei.refdata
+from espei.utils import extract_aliases
+
+TRACE = 15
+
+
+def _get_ser_data(element, ref_state, fallback_ref_state="SGTE91") -> Dict[str, Union[str, float]]:
+    """Return a dictionary of the stable element reference (SER) data.
+
+    If no SER data is found, returns an empty dictionary.
+    """
+    ser_ref_state = ref_state + "SER"
+    # Return an empty dict for backwards compatibility, since the SER data may not exist
+    ser_dict = getattr(espei.refdata, ser_ref_state, {})
+    fallback_ser_ref_state = fallback_ref_state + "SER"
+    fallback_ser_dict = getattr(espei.refdata, fallback_ser_ref_state)
+    el_ser_data = ser_dict.get(element)
+    if el_ser_data is None and ref_state == fallback_ref_state:
+        # No data found, no fallback alternative
+        logging.warning("%s has no entry in the %s reference data. Fitting formation energies will not be possible.", element, ser_ref_state)
+    elif el_ser_data is None:
+        # No data found, try the fallback
+        el_ser_data = fallback_ser_dict.get(element)
+        if el_ser_data is None:
+            # No data found in the fallback
+            logging.warning("%s has no entry in the %s reference data nor in the %s fallback reference data. Fitting formation energies will not be possible.", element, ser_ref_state + "SER", fallback_ser_ref_state)
+            return {}
+        else:
+            # Data found in the fallback
+            logging.log(TRACE, "%s has no entry in the %s reference data, but was available in the %s fallback reference data.", element, ser_ref_state + "SER", fallback_ser_ref_state)
+    if el_ser_data is not None:
+        return el_ser_data
+    else:
+        return {}
+
+
+def initialize_database(phase_models, ref_state, dbf=None, fallback_ref_state="SGTE91"):
+    """Return a Database boostraped with elements, species, phases and unary lattice stabilities.
+
+    Parameters
+    ----------
+    phase_models : Dict[str, Any]
+        Dictionary of components and phases to fit.
+    ref_state : str
+        String of the reference data to use, e.g. 'SGTE91' or 'SR2016'
+    dbf : Optional[Database]
+        Initial pycalphad Database that can have parameters that would not be fit by ESPEI
+    fallback_ref_state : str
+        String of the reference data to use for SER data, defaults to 'SGTE91'
+
+    Returns
+    -------
+    Database
+        A new pycalphad Database object, or a modified one if it was given.
+
+    """
+    if dbf is None:
+        dbf = Database()
+    lattice_stabilities = getattr(espei.refdata, ref_state)
+    ser_stability = getattr(espei.refdata, ref_state + "Stable")
+    aliases = extract_aliases(phase_models)
+    phases = sorted({ph.upper() for ph in phase_models["phases"].keys()})
+    elements = {el.upper() for el in phase_models["components"]}
+    dbf.elements.update(elements)
+    dbf.species.update({v.Species(el, {el: 1}, 0) for el in elements})
+    
+    # Add SER reference data for this element
+    for element in dbf.elements:
+        if element in dbf.refstates:
+            continue  # Do not clobber user reference states
+        el_ser_data = _get_ser_data(element, ref_state, fallback_ref_state=fallback_ref_state)
+        # Try to look up the alias that we are using in this fitting
+        el_ser_data["phase"] = aliases.get(el_ser_data["phase"], el_ser_data["phase"])
+        if el_ser_data["phase"] not in phases:
+            # We have the Gibbs energy expression that we need in the reference
+            # data, but this phase is not a candidate in the phase models. The
+            # phase won't be added to the database, so looking up the phases's
+            # energy won't work.
+            logging.warning(
+                "The reference phase for %s, %s, is not in the supplied phase models and won't be "
+                "added to the Database phases. Fitting formation energies will not be possible.",
+                element, el_ser_data["phase"]
+            )
+        dbf.refstates[element] = el_ser_data
+
+    # Add the phases
+    for phase_name, phase_data in phase_models['phases'].items():
+        # TODO: Need to support model hints for: magnetic, order-disorder, etc.
+        site_ratios = phase_data['sublattice_site_ratios']
+        subl_model = phase_data['sublattice_model']
+        if phase_name not in dbf.phases.keys():  # Do not clobber user phases
+            dbf.add_phase(phase_name, dict(), site_ratios)
+            dbf.add_phase_constituents(phase_name, subl_model)
+    
+    # Add the GHSER functions to the Database
+    for element in dbf.elements:
+        # Use setdefault here to not clobber user-provided functions
+        if element == "VA":
+            dbf.symbols.setdefault("GHSERVA", 0)
+        else:
+            # note that `c.upper()*2)[:2]` returns "AL" for "Al" and "BB" for "B"
+            # Using this ensures that GHSER functions will be unique, e.g.
+            # GHSERC would be an abbreviation for GHSERCA.
+            sym_name = "GHSER" + (element.upper()*2)[:2]
+            dbf.symbols.setdefault(sym_name, 0)
+    return dbf

tests/test_database_utils.py
@@ -1,0 +1,99 @@
+"""Test ESPEI's database utilities
+
+Tests in here are heavily parameterized and represent a large fraction of the
+number of tests, but a small amount of coverage.
+"""
+
+from pycalphad import variables as v
+import pytest
+import sympy
+from sympy import Piecewise, Symbol
+
+import espei.refdata
+from espei.database_utils import initialize_database, _get_ser_data
+
+from .testing_data import SGTE91_PURE_ELEMENTS
+
+
+@pytest.mark.parametrize("element_name", SGTE91_PURE_ELEMENTS)
+def test_get_ser_data_SGTE91(element_name):
+    """Test that all SGTE91 elements can be read from the primary SGTE91 dataset without error"""
+    # Make a fake fallback dataset so we can confirm that it's pulling from the primary
+    FAKE_FALLBACK = "FAKE_FALLBACK_"
+    setattr(espei.refdata, FAKE_FALLBACK + "SER", {})
+    data = _get_ser_data(element_name, "SGTE91", fallback_ref_state=FAKE_FALLBACK)
+    assert len(data) > 0
+    assert isinstance(data['phase'], str)
+    assert isinstance(data['H298'], float)
+    assert isinstance(data['S298'], float)
+    assert isinstance(data['mass'], float)
+    delattr(espei.refdata, FAKE_FALLBACK + "SER")
+
+
+@pytest.mark.parametrize("element_name", SGTE91_PURE_ELEMENTS)
+def test_get_ser_data_falls_back_on_SGTE91(element_name):
+    """Test that a reference dataset with no SER data falls back on SGTE91"""
+    data = _get_ser_data(element_name, "FAKE_REF_STATE")
+    assert len(data) > 0
+    assert isinstance(data['phase'], str)
+    assert isinstance(data['H298'], float)
+    assert isinstance(data['S298'], float)
+    assert isinstance(data['mass'], float)
+
+
+def test_get_ser_data_is_successful_without_refdata():
+    """Test that an element not in reference data or fallback data returns an empty dict"""
+    assert _get_ser_data("FAKE ELEMENT", "SGTE91") == {}
+    assert _get_ser_data("FAKE ELEMENT", "FAKE REF DATA") == {}
+
+
+def test_database_initialization_custom_refstate():
+    """Test that a custom reference state with ficticious pure elements can be used to construct a Database"""
+    refdata_stable = {
+        "Q": Piecewise((sympy.oo, True)),
+        "ZX": Piecewise((sympy.oo, True)),
+    }
+    refdata = {
+        ("Q", "ALPHA"): Symbol("GHSERQQ"),
+        ("Q", "BETA"): Symbol("GHSERQQ") + 10000.0,
+        ("ZX", "BETA"): Symbol("GHSERZX"),
+    }
+    refdata_ser = {
+        'Q': {'phase': 'ALPHA', 'mass': 8.0, 'H298': 80.0, 'S298': 0.80},
+        'ZX': {'phase': 'BETA', 'mass': 52.0, 'H298': 520.0, 'S298': 5.20},
+    }
+
+    # Setup refdata
+    CUSTOM_REFDATA_NAME = "CUSTOM"
+    setattr(espei.refdata, CUSTOM_REFDATA_NAME + "Stable", refdata_stable)
+    setattr(espei.refdata, CUSTOM_REFDATA_NAME, refdata)
+    setattr(espei.refdata, CUSTOM_REFDATA_NAME + "SER", refdata_ser)
+
+    # Test
+    phase_models = {
+        "components": ["Q", "ZX"],
+        "phases": {
+            "ALPHA": {
+                "sublattice_model": [["Q"]],
+                "sublattice_site_ratios": [1],
+            },
+            "BCC": {
+                "aliases": ["BETA"],
+                "sublattice_model": [["Q", "ZX"]],
+                "sublattice_site_ratios": [1.0],
+            },
+        }
+    }
+    dbf = initialize_database(phase_models, CUSTOM_REFDATA_NAME)
+    assert set(dbf.phases.keys()) == {"ALPHA", "BCC"}
+    assert dbf.elements == {"Q", "ZX"}
+    assert dbf.species == {v.Species("Q"), v.Species("ZX")}
+    assert 'GHSERQQ' in dbf.symbols
+    assert 'GHSERZX' in dbf.symbols
+    assert dbf.refstates["Q"]["phase"] == "ALPHA"
+    assert dbf.refstates["ZX"]["phase"] == "BCC"
+
+    # Teardown refdata
+    delattr(espei.refdata, CUSTOM_REFDATA_NAME + "Stable")
+    delattr(espei.refdata, CUSTOM_REFDATA_NAME)
+    delattr(espei.refdata, CUSTOM_REFDATA_NAME + "SER")

espei/paramselect.py
@@ -24,9 +24,9 @@ import numpy as np
 import sympy
 from tinydb import where
 from pycalphad import Database, Model, variables as v
-from pycalphad.io.database import Species
 
 import espei.refdata
+from espei.database_utils import initialize_database
 from espei.core_utils import get_data, get_samples, get_weights
 from espei.parameter_selection.model_building import build_candidate_models
 from espei.parameter_selection.selection import select_model
@@ -34,7 +34,7 @@ from espei.parameter_selection.ternary_parameters import build_ternary_feature_m
 from espei.parameter_selection.utils import get_data_quantities, feature_transforms
 from espei.sublattice_tools import generate_symmetric_group, generate_interactions, \
     tuplify, interaction_test, endmembers_from_interaction, generate_endmembers
-from espei.utils import PickleableTinyDB, sigfigs
+from espei.utils import PickleableTinyDB, sigfigs, extract_aliases
 
 
 TRACE = 15  # TRACE logging level
@@ -277,7 +277,7 @@ def fit_ternary_interactions(dbf, phase_name, symmetry, endmembers, datasets, ri
                     dbf.add_parameter('L', phase_name, tuple(map(tuplify, syminter)), degree, degree_polys[degree])
 
 
-def phase_fit(dbf, phase_name, symmetry, subl_model, site_ratios, datasets, refdata, ridge_alpha, aicc_penalty=None, aliases=None):
+def phase_fit(dbf, phase_name, symmetry, datasets, refdata, ridge_alpha, aicc_penalty=None, aliases=None):
     """Generate an initial CALPHAD model for a given phase and sublattice model.
 
     Parameters
@@ -288,10 +288,6 @@ def phase_fit(dbf, phase_name, symmetry, subl_model, site_ratios, datasets, refd
         Name of the phase.
     symmetry : [[int]]
         Sublattice model symmetry.
-    subl_model : [[str]]
-        Sublattice model for the phase of interest.
-    site_ratios : [float]
-        Number of sites in each sublattice, normalized to one atom.
     datasets : PickleableTinyDB
         All datasets to consider for the calculation.
     refdata : dict
@@ -302,9 +298,8 @@ def phase_fit(dbf, phase_name, symmetry, subl_model, site_ratios, datasets, refd
         with ordinary least squares regression. For now, the parameter is applied to all features.
     aicc_penalty : dict
         Map of phase name to feature to a multiplication factor for the AICc's parameter penalty.
-    aliases : [str]
-        Alternative phase names. Useful for matching against
-        reference data or other datasets. (Default value = None)
+    aliases : Dict[str, str]
+        Mapping of possible aliases to the Database phase names.
 
     Returns
     -------
@@ -316,14 +311,22 @@ def phase_fit(dbf, phase_name, symmetry, subl_model, site_ratios, datasets, refd
     aicc_phase_penalty = aicc_penalty.get(phase_name, {})
     if not hasattr(dbf, 'varcounter'):
         dbf.varcounter = 0
+    phase_obj = dbf.phases[phase_name]
+    # TODO: assumed pure elements - add proper support for Species objects
+    subl_model = [sorted([sp.name for sp in subl]) for subl in phase_obj.constituents]
+    site_ratios = phase_obj.sublattices
     # First fit endmembers
     all_em_count = len(generate_endmembers(subl_model))  # number of total endmembers
     endmembers = generate_endmembers(subl_model, symmetry)
     # Number of significant figures in parameters, might cause rounding errors
     numdigits = 6
     em_dict = {}
-    aliases = [] if aliases is None else aliases
-    aliases = sorted(set(aliases + [phase_name]))
+    # TODO: use the global aliases dictionary passed in as-is instead of converting it to a phase-local dict
+    # TODO: use the aliases dictionary in dataset queries to find relevant data
+    if aliases is None:
+        aliases = [phase_name]
+    else:
+        aliases = sorted([alias for alias, database_phase in aliases.items() if database_phase == phase_name])
     logging.info('FITTING: {}'.format(phase_name))
     logging.log(TRACE, '{0} endmembers ({1} distinct by symmetry)'.format(all_em_count, len(endmembers)))
 
@@ -464,53 +467,12 @@ def generate_parameters(phase_models, datasets, ref_state, excess_model, ridge_a
     """
     logging.info('Generating parameters.')
     logging.log(TRACE, f'Found the following user reference states: {espei.refdata.INSERTED_USER_REFERENCE_STATES}')
-    phases = sorted(map(lambda x: x.upper(), phase_models['phases'].keys()))
-    dbf = dbf or Database()
-    dbf.elements.update(set(phase_models['components']))
-    for el in dbf.elements:
-        dbf.species.add(Species(el, {el: 1}, 0))
-        # Add the SER reference data
-        dbf.refstates[el] = espei.refdata.ser_dict[el]
-        # update the refdata for this element with the reference phase
-        if el not in espei.refdata.pure_element_phases.keys():
-            # Probably VA, /- or something else
-            continue
-        refdata_phase = espei.refdata.pure_element_phases[el]
-        if refdata_phase in phases:
-            dbf.refstates[el]['phase'] = refdata_phase
-        else:
-            # Check all the aliases and set the one that matches
-            for phase_name, phase_obj in phase_models['phases'].items():
-                for alias in phase_obj.get('aliases', []):
-                    if alias == refdata_phase:
-                        dbf.refstates[el]['phase'] = phase_name
-    # Write reference state to Database
     refdata = getattr(espei.refdata, ref_state)
-    stabledata = getattr(espei.refdata, ref_state + 'Stable')
-    for key, element in refdata.items():
-        if isinstance(element, sympy.Piecewise):
-            newargs = element.args + ((0, True),)
-            refdata[key] = sympy.Piecewise(*newargs)
-    for key, element in stabledata.items():
-        if isinstance(element, sympy.Piecewise):
-            newargs = element.args + ((0, True),)
-            stabledata[key] = sympy.Piecewise(*newargs)
-    comp_refs = {c.upper(): stabledata[c.upper()] for c in dbf.elements if c.upper() != 'VA'}
-    comp_refs['VA'] = 0
-    # note that the `c.upper()*2)[:2]` returns 'AL' for c.upper()=='AL' and 'VV' for c.upper()=='V'
-    dbf.symbols.update({'GHSER' + (c.upper()*2)[:2]: data for c, data in comp_refs.items()})
-    for phase_name, phase_obj in sorted(phase_models['phases'].items(), key=operator.itemgetter(0)):
-        # Perform parameter selection and single-phase fitting based on input
-        # TODO: Need to pass particular models to include: magnetic, order-disorder, etc.
-        symmetry = phase_obj.get('equivalent_sublattices', None)
-        aliases = phase_obj.get('aliases', None)
-        # TODO: More advanced phase data searching
-        site_ratios = phase_obj['sublattice_site_ratios']
-        subl_model = phase_obj['sublattice_model']
-        if phase_name not in dbf.phases.keys():
-            dbf.add_phase(phase_name, dict(), site_ratios)
-            dbf.add_phase_constituents(phase_name, subl_model)
-            dbf.add_structure_entry(phase_name, phase_name)
-        phase_fit(dbf, phase_name, symmetry, subl_model, site_ratios, datasets, refdata, ridge_alpha, aicc_penalty=aicc_penalty_factor, aliases=aliases)
+    aliases = extract_aliases(phase_models)
+    dbf = initialize_database(phase_models, ref_state, dbf)
+    # Fit phases in alphabetic order so the VV#### counter is constistent between runs
+    for phase_name, phase_data in sorted(phase_models['phases'].items(), key=operator.itemgetter(0)):
+        symmetry = phase_data.get('equivalent_sublattices', None)
+        phase_fit(dbf, phase_name, symmetry, datasets, refdata, ridge_alpha, aicc_penalty=aicc_penalty_factor, aliases=aliases)
     logging.info('Finished generating parameters.')
     return dbf

espei/utils.py
@@ -6,13 +6,11 @@ Classes and functions defined here should have some reuse potential.
 
 import itertools
 import re
-import os
 from collections import namedtuple
 
 import bibtexparser
 import numpy as np
 import sympy
-import dask
 from bibtexparser.bparser import BibTexParser
 from bibtexparser.customization import convert_to_unicode
 from distributed import Client
@@ -439,3 +437,32 @@ def popget(d, key, default=None):
         return d.pop(key)
     except KeyError:
         return default
+
+
+def extract_aliases(phase_models):
+    """Map possible phase name aliases to the desired phase model phase name.
+    
+    This function enforces that each alias is only claimed by one phase.
+    Each phase name in the phase models is added as an alias for itself to
+    support an "identity" operation.
+
+    Parameters
+    ----------
+    phase_models
+        Phase models ESPEI uses to initialize databases. Must contain a mapping
+        of phase names to phase data (sublattices, site ratios, aliases)
+
+    Returns
+    -------
+    Dict[str, str]
+
+    """
+    # Intialize aliases with identity for each phase first
+    aliases = {phase_name: phase_name for phase_name in phase_models["phases"].keys()}
+    for phase_name, phase_dict in phase_models["phases"].items():        
+        for alias in phase_dict.get("aliases", []):
+            if alias not in aliases:
+                aliases[alias] = phase_name
+            else:
+                raise ValueError(f"Cannot add {alias} as an alias for {phase_name} because it is already used by {aliases[alias]}")
+    return aliases

tests/test_utils.py
@@ -3,9 +3,11 @@ Test espei.utils classes and functions.
 """
 import pickle
 
+import pytest
 from tinydb import where
 from espei.utils import ImmediateClient, PickleableTinyDB, MemoryStorage, \
-    flexible_open_string, add_bibtex_to_bib_database, bib_marker_map
+    flexible_open_string, add_bibtex_to_bib_database, bib_marker_map, \
+    extract_aliases
 
 from .fixtures import datasets_db, tmp_file
 from .testing_data import CU_MG_TDB
@@ -101,3 +103,59 @@ def test_bib_marker_map():
     }
     assert EXEMPLAR_DICT == marker_dict
 
+
+@pytest.mark.parametrize('reason, phase_models, expected_aliases', [
+    (
+        "No phases should give no aliases",
+        {"phases": {}},
+        {}
+    ),
+    (
+        "A phase has an alias for itself and works without given aliases",
+        {"phases": {"ALPHA": {}}},
+        {"ALPHA": "ALPHA"}
+    ),
+    (
+        "Empty aliases list works",
+        {"phases": {"ALPHA": {"aliases": []}}},
+        {"ALPHA": "ALPHA"}
+    ),
+    (
+        "Basic test for adding aliases correctly",
+        {"phases": {
+            "ALPHA": {"aliases": ["FCC_A1"]}
+        }},
+        {"ALPHA": "ALPHA", "FCC_A1": "ALPHA"}
+    ),
+    (
+        "A phase can have mulitple aliases",
+        {"phases": {
+            "ALPHA": {"aliases": ["FCC_A1", "A1", "FCC"]}
+        }},
+        {"ALPHA": "ALPHA", "FCC_A1": "ALPHA", "FCC": "ALPHA", "A1": "ALPHA"}
+    ),
+    (
+        "Cannot have two phases with the same alias",
+        {"phases": {
+            "ALPHA": {"aliases": ["FCC_A1"]},
+            "GAMMA": {"aliases": ["FCC_A1"]},
+        }},
+        None
+    ),
+    (
+        "Cannot have a prescribed phase as an alias",
+        {"phases": {
+            "ALPHA": {"aliases": ["BETA"]},
+            "BETA": {"aliases": []},
+        }},
+        None
+    ),
+]
+)
+def test_extract_aliases(reason, phase_models, expected_aliases):
+    if expected_aliases is None:
+        with pytest.raises(ValueError):
+            aliases = extract_aliases(phase_models)
+            print(aliases)
+    else:
+        assert extract_aliases(phase_models) == expected_aliases, reason

tests/testing_data.py
@@ -1128,3 +1128,13 @@ LI_SN_LIQUID_EQ_DATA = {
     "values": [[[0, 0, 0, 0]]],
     "reference": "equilibrium thermochemical tests", "comment": "Valid speices: LI, LI4SN, SN; No interaction for LI/LI4SN, but there is one for LI4SN/SN. True values should be [0.0, -28133.588, -40049.995, 0.0]"
 }
+
+SGTE91_PURE_ELEMENTS = (
+    '/-', 'VA', 'AG', 'AL', 'AM', 'AS', 'AU', 'B', 'BA', 'BE', 'BI', 'C', 'CA',
+     'CD', 'CE', 'CO', 'CR', 'CS', 'CU', 'DY', 'ER', 'EU', 'FE', 'GA', 'GD',
+     'GE', 'HF', 'HG', 'HO', 'IN', 'IR', 'K', 'LA', 'LI', 'LU', 'MG', 'MN',
+     'MO', 'N', 'NA', 'NB', 'ND', 'NI', 'NP', 'O', 'OS', 'P', 'PA', 'PB', 'PD',
+      'PR', 'PT', 'PU', 'RB', 'RE', 'RH', 'RU', 'S', 'SB', 'SC', 'SE', 'SI',
+      'SM', 'SN', 'SR', 'TA', 'TB', 'TC', 'TE', 'TH', 'TI', 'TL', 'TM', 'U',
+      'V', 'W', 'Y', 'YB', 'ZN', 'ZR',
+)

commit e67b222ff8ee00c8ac74db3e1617a982073abc8d

FIX: Fix setting GHSER__ to zero in database initialization (#161)
Fixes a bug introduced by #158 where GHSER data was set to zero. Includes a test.

espei/database_utils.py
@@ -111,5 +111,5 @@ def initialize_database(phase_models, ref_state, dbf=None, fallback_ref_state="S
             # Using this ensures that GHSER functions will be unique, e.g.
             # GHSERC would be an abbreviation for GHSERCA.
             sym_name = "GHSER" + (element.upper()*2)[:2]
-            dbf.symbols.setdefault(sym_name, 0)
+            dbf.symbols.setdefault(sym_name, ser_stability[element])
     return dbf

tests/test_database_utils.py
@@ -97,3 +97,23 @@ def test_database_initialization_custom_refstate():
     delattr(espei.refdata, CUSTOM_REFDATA_NAME + "Stable")
     delattr(espei.refdata, CUSTOM_REFDATA_NAME)
     delattr(espei.refdata, CUSTOM_REFDATA_NAME + "SER")
+
+
+def test_database_initialization_adds_GHSER_data():
+    phase_models = {
+        "components": ["CR", "NI"],
+        "phases": {
+            "FCC_A1": {
+                "sublattice_model": [["CR", "NI"]],
+                "sublattice_site_ratios": [1],
+            },
+            "BCC": {
+                "aliases": ["BCC_A2"],
+                "sublattice_model": [["CR", "NI"]],
+                "sublattice_site_ratios": [1.0],
+            },
+        }
+    }
+    dbf = initialize_database(phase_models, "SGTE91")
+    assert dbf.symbols["GHSERCR"] != sympy.S.Zero
+    assert dbf.symbols["GHSERNI"] != sympy.S.Zero