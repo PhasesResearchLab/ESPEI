0cffaa81f48fec86547177dace3a88786e48c059
bocklund <brandonbocklund@gmail.com>
2021-05-03 23:35:29 -0400

    Fix data markers

--- espei/plot.py
+++ espei/plot.py
@@ -608,7 +608,7 @@ def plot_interaction(dbf, comps, phase_name, configuration, output, datasets=Non
         dataplot_kwargs.setdefault('clip_on', False)
         dataplot_kwargs.setdefault('label', symbol_map[ref]['formatted'])
         dataplot_kwargs.setdefault('marker', symbol_map[ref]['markers']['marker'])
-        dataplot_kwargs.setdefault('fillstyle', mark['fillstyle'])
+        dataplot_kwargs.setdefault('fillstyle', symbol_map[ref]['markers']['fillstyle'])
         ax.plot(indep_var_data, response_data, **dataplot_kwargs)
     ax.set_xlim((0, 1))
     ax.set_xlabel(str(':'.join(endpoints[0])) + ' to ' + str(':'.join(endpoints[1])))
