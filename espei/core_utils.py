dda96a979b47da8710f857699bfb4006aef717b8
bocklund <brandonbocklund@gmail.com>
2021-05-11 10:44:46 -0400

    Add filter_configurations and filter_temperatures

--- espei/core_utils.py
+++ espei/core_utils.py
@@ -68,6 +68,78 @@ def get_data(comps, phase_name, configuration, symmetry, datasets, prop):
     return desired_data
 
 
+def filter_configurations(desired_data, configuration, symmetry):
+    """
+    Return a copy of non-equilibrium thermochemical datasets with invalid configurations removed.
+
+    Parameters
+    ----------
+    desired_data : List[Dict[str, Any]]
+        List of non-equilibrium thermochemical datasets
+    configuration : tuple
+        Sublattice configuration as a tuple, e.g. ("CU", ("CU", "MG"))
+    symmetry : list of lists
+        List of sublattice indices with symmetry
+
+    Returns
+    -------
+    List[Dict[str, Any]]
+
+    """
+    # TODO: can this copy be removed safely?
+    # This seems to be necessary because the 'values' member does not modify 'datasets'
+    # But everything else does!
+    desired_data = copy.deepcopy(desired_data)
+
+    for idx, data in enumerate(desired_data):
+        # Filter output values to only contain data for matching sublattice configurations
+        matching_configs = np.array([(canonicalize(sblconf, symmetry) == canonicalize(configuration, symmetry))
+                                     for sblconf in data['solver']['sublattice_configurations']])
+        matching_configs = np.arange(len(data['solver']['sublattice_configurations']))[matching_configs]
+        # Rewrite output values with filtered data
+        desired_data[idx]['values'] = np.array(data['values'], dtype=np.float_)[..., matching_configs]
+        desired_data[idx]['solver']['sublattice_configurations'] = recursive_tuplify(np.array(data['solver']['sublattice_configurations'],
+                                                                                              dtype=np.object_)[matching_configs].tolist())
+        try:
+            desired_data[idx]['solver']['sublattice_occupancies'] = np.array(data['solver']['sublattice_occupancies'],
+                                                                             dtype=np.object_)[matching_configs].tolist()
+        except KeyError:
+            pass
+    return desired_data
+
+
+def filter_temperatures(desired_data):
+    """
+    Return a copy of non-equilibrium thermochemical datasets with temperatures below 298.15 K removed.
+
+    The currently provided unary reference data from ESPEI use the SGTE unary data that
+    is defined as piecewise in temperature with a lower limit of 298.15 K for most
+    elements. Since pycalphad does not extrapolate outside of piecewise temperature
+    limits, this filter prevents fitting data to regions of temperature space where
+    the energy is zero.
+
+    Parameters
+    ----------
+    desired_data : List[Dict[str, Any]]
+        List of non-equilibrium thermochemical datasets
+
+    Returns
+    -------
+    List[Dict[str, Any]]
+
+    """
+    # TODO: can this copy be removed safely?
+    # This seems to be necessary because the 'values' member does not modify 'datasets'
+    # But everything else does!
+    desired_data = copy.deepcopy(desired_data)
+
+    for idx, data in enumerate(desired_data):
+        temp_filter = np.atleast_1d(data['conditions']['T']) >= 298.15
+        desired_data[idx]['conditions']['T'] = np.atleast_1d(data['conditions']['T'])[temp_filter]
+        desired_data[idx]['values'] = np.array(data['values'], dtype=np.float_)[..., temp_filter, :].tolist()
+    return desired_data
+
+
 def _zpf_conditions_shape(zpf_values):
     """
     Calculate the shape of the conditions for ZPF values
