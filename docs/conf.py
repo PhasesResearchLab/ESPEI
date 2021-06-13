3d9440ce19969911f6d293531d8beccd0526f264
bocklund <brandonbocklund@gmail.com>
2021-06-13 12:14:23 -0400

    Add custom css to enlarge font

--- docs/conf.py
+++ docs/conf.py
@@ -114,6 +114,11 @@ html_logo = '_static/ESPEI-logo-withtext@200px.png'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
+# These paths are either relative to html_static_path
+# or fully qualified paths (eg. https://...)
+html_css_files = [
+    'css/custom.css',
+]
 
 # -- Options for HTMLHelp output ------------------------------------------
 
