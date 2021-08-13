8962a6b39ccc3800a602da050988a35ce65f8b87
Brandon Bocklund <brandonbocklund@gmail.com>
2021-08-13 17:16:17 -0400

    MAINT: Bump minimum pycalphad to 0.9.2 (#209)

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
         'importlib_metadata',  # drop for Python>=3.8
         'matplotlib',
         'numpy>=1.20',
-        'pycalphad>=0.9.0',
+        'pycalphad>=0.9.2',
         'pyyaml',
         'setuptools_scm[toml]>=6.0',
         'scikit-learn',
