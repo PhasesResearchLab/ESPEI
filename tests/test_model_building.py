fbcb2f31c7e4254c37328c142f671e54eb0d2b91
Brandon Bocklund <bocklund1@llnl.gov>
2022-02-21 13:46:49 -0800

    ENH: Performance improvements in parameter selection (#225)
    This PR implements several performance improvements guided by profiling, leading to speedups between 4x-40x (depending on the sublattices models and data)
    
    1. `build_feature_sets` is now cached, as it is frequently called in an inner loop with the same arguments and is relatively expensive due to the combinatorial nature of making products of temperature and composition features. `build_feature_sets` is also slightly refactored to build the candidates (product of temperature and composition features), rather than returning tuples of temperature features and composition features. Required some minor test changes to accommodate new ordering and the requirement that the arguments can be multiplied together.
    2. Model building can be expensive if called in a tight loop, so there are changes to `fit_formation_energy` to delay Model instantiation until there are data and therefore parameters to fit.
    3. Build an `OrderedDict` of sorted parameters returned from `fit_formation_energy` instead of sorting repeatedly in the inner loop over the parameters, since casting `SymPy` symbols to strings is relatively expensive.
    4. When calling `phase_fit`, pass (a new tinydb of) datasets that match the phase to be fit. We know all the other phase data is irrelevant and it wastes time in tinydb to perform the queries on documents that are impossible to match (because they belong to another phase).
    5. `generate_interactions` now disallows cross-sublattice interactions by default (unless the model has symmetry). Cross interactions are very rare for databases "in the wild" and are hard to reason about and it is very expensive to get `sorted_interactions` for multi-component, multi-sublattice systems due to combinatorics. Users can recover the old behavior by adding symmetry to their phase models where each sublattice is symmetric to itself (i.e. a symmetry no-op). The `generate_interactions` function is also rewritten to be a little more readable.

--- tests/test_model_building.py
+++ tests/test_model_building.py
@@ -13,36 +13,20 @@ from espei.sublattice_tools import generate_symmetric_group, sorted_interactions
 
 def test_build_feature_sets_generates_desired_binary_features_for_cp_like():
     """Binary feature sets can be correctly generated for heat capacity-like features"""
-    binary_temp_features = ['TlogT', 'T**2', '1/T', 'T**3']
-    binary_excess_features= ['YS', 'YS*Z', 'YS*Z**2', 'YS*Z**3']
-    feat_sets = build_feature_sets(binary_temp_features, binary_excess_features)
+    YS = symengine.Symbol("YS")
+    Z = symengine.Symbol("Z")
+    temp_features = [v.T, v.T**2, 1/v.T, v.T**3]
+    excess_features= [YS, YS*Z, YS*Z**2, YS*Z**3]
+    feat_sets = build_feature_sets(temp_features, excess_features)
     assert len(feat_sets) == 340
-    assert feat_sets[0] == ((['TlogT'], 'YS'),)
-    assert feat_sets[5] == ((['TlogT'], 'YS'), (['TlogT', 'T**2'], 'YS*Z'))
-    assert feat_sets[-1] == ((['TlogT', 'T**2', '1/T', 'T**3'], 'YS'), (['TlogT', 'T**2', '1/T', 'T**3'], 'YS*Z'), (['TlogT', 'T**2', '1/T', 'T**3'], 'YS*Z**2'), (['TlogT', 'T**2', '1/T', 'T**3'], 'YS*Z**3'))
-
-
-def test_build_feature_sets_generates_desired_binary_features_for_h_like():
-    """Binary feature sets can be correctly generated for enthalpy-like models"""
-    binary_temp_features = ['1']
-    binary_excess_features= ['YS', 'YS*Z', 'YS*Z**2', 'YS*Z**3']
-    feat_sets = build_feature_sets(binary_temp_features, binary_excess_features)
-    assert len(feat_sets) == 4
-    assert feat_sets[0] == ((['1'], 'YS'),)
-    assert feat_sets[1] == ((['1'], 'YS'), (['1'], 'YS*Z'))
-    assert feat_sets[2] == ((['1'], 'YS'), (['1'], 'YS*Z'), (['1'], 'YS*Z**2'))
-    assert feat_sets[3] == ((['1'], 'YS'), (['1'], 'YS*Z'), (['1'], 'YS*Z**2'), (['1'], 'YS*Z**3'))
-
-
-def test_build_feature_sets_generates_desired_ternary_features():
-    """Ternary feature sets can be correctly generated"""
-    ternary_temp_features = ['1']
-    ternary_excess_features= [('YS',), ('YS*V_I', 'YS*V_J', 'YS*V_K')]
-    feat_sets = build_feature_sets(ternary_temp_features, ternary_excess_features)
-    assert len(feat_sets) == 2
-    assert feat_sets[0] == ((['1'], ('YS',)),)
-    assert feat_sets[1] == ((['1'], ('YS',)), (['1'], ('YS*V_I', 'YS*V_J', 'YS*V_K')))
-
+    assert feat_sets[0] == [v.T*YS]
+    assert feat_sets[5] == [v.T*YS, v.T*YS*Z, v.T**2*YS*Z]
+    assert feat_sets[-1] == [
+        v.T * YS,        v.T**2 * YS,        1/v.T * YS,        v.T**3 * YS,
+        v.T * YS * Z,    v.T**2 * YS * Z,    1/v.T * YS * Z,    v.T**3 * YS * Z,
+        v.T * YS * Z**2, v.T**2 * YS * Z**2, 1/v.T * YS * Z**2, v.T**3 * YS * Z**2,
+        v.T * YS * Z**3, v.T**2 * YS * Z**3, 1/v.T * YS * Z**3, v.T**3 * YS * Z**3,
+        ]
 
 
 def test_binary_candidate_models_are_constructed_correctly():
@@ -118,7 +102,7 @@ def test_ternary_candidate_models_are_constructed_correctly():
             [v.T*YS*symengine.log(v.T)],
             [v.T*YS*symengine.log(v.T), v.T**2*YS],
             [v.T*V_I*YS*symengine.log(v.T), v.T*V_J*YS*symengine.log(v.T), v.T*V_K*YS*symengine.log(v.T)],
-            [v.T*V_I*YS*symengine.log(v.T), v.T*V_J*YS*symengine.log(v.T), v.T*V_K*YS*symengine.log(v.T), v.T**2*V_I*YS, v.T**2*V_J*YS, v.T**2*V_K*YS],
+            [v.T*V_I*YS*symengine.log(v.T), v.T**2*V_I*YS, v.T*V_J*YS*symengine.log(v.T), v.T**2*V_J*YS, v.T*V_K*YS*symengine.log(v.T), v.T**2*V_K*YS],
         ]),
         ('SM_FORM', [
             [v.T*YS],
