179e73da86780cab4d2b74e15bcf64e9cf619734
bocklund <brandonbocklund@gmail.com>
2022-02-21 15:53:02 -0800

    Bump CI and metadata to 3.10

--- setup.py
+++ setup.py
@@ -64,6 +64,7 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
     ],
     entry_points={'console_scripts': [
                   'espei = espei.espei_script:main']}
