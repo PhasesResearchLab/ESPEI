f8977de8af8baa40571e619140304f3b077a1c87
Brandon Bocklund <brandonbocklund@gmail.com>
2022-02-21 16:06:54 -0800

    MAINT: Bump minimum pycalphad to 0.10.0 and support Python 3.10 (#227)

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
         'importlib_metadata',  # drop for Python>=3.8
         'matplotlib',
         'numpy>=1.20',
-        'pycalphad>=0.9.2',
+        'pycalphad>=0.10.0',
         'pyyaml',
         'setuptools_scm[toml]>=6.0',
         'scikit-learn>=1.0',
@@ -64,6 +64,7 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
     ],
     entry_points={'console_scripts': [
                   'espei = espei.espei_script:main']}
