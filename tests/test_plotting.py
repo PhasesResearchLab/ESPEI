PhasesResearchLab/ESPEI

commit 80179160d3aa830449e4bd5e7ead5c8222046834

FIX: Support varying pressure and P-X plots in dataplot() and ravel_zpf_values() (#240)

espei/plot.py
@@ -23,6 +23,7 @@ from espei.utils import build_sitefractions
 
 plot_mapping = {
     'T': 'Temperature (K)',
+    'P': 'Pressure (Pa)',
     'CPM': 'Heat Capacity (J/K-mol-atom)',
     'HM': 'Enthalpy (J/mol-atom)',
     'SM': 'Entropy (J/K-mol-atom)',
@@ -251,13 +252,11 @@ def dataplot(comps, phases, conds, datasets, tielines=True, ax=None, plot_kwargs
     legend_handles, phase_color_map = phase_legend(phases_without_hyperplane)
     # Force hyperplane color to black
     phase_color_map["__HYPERPLANE__"] = "black"
-    
+
     if projection is None:
         # TODO: There are lot of ways this could break in multi-component situations
-
-        # plot x vs. T
-        y = 'T'
-
+        # plot x (X) vs. y (T/P)
+        y = str(y)
         # handle plotting kwargs
         scatter_kwargs = {'markersize': 6, 'markeredgewidth': 1}
         # raise warnings if any of the aliased versions of the default values are used
@@ -267,7 +266,10 @@ def dataplot(comps, phases, conds, datasets, tielines=True, ax=None, plot_kwargs
                 warnings.warn("'{0}' passed as plotting keyword argument to dataplot, but the alias '{1}' is already set to '{2}'. Use the full version of the keyword argument '{1}' to override the default.".format(aliased_arg, actual_arg, scatter_kwargs.get(actual_arg)))
         scatter_kwargs.update(plot_kwargs)
 
-        eq_dict = ravel_zpf_values(desired_data, [x])
+        # fixed_pot_conds used to filter the ZPF values for ones that match the
+        # caller specified conditions (via floating point equality [==])
+        fixed_pot_conds = {str(key): val for key, val in conds.items() if ((key == v.T) or (key == v.P)) and len(np.atleast_1d(val)) == 1}
+        eq_dict = ravel_zpf_values(desired_data, [x], conditions=fixed_pot_conds)
         updated_tieline_plot_kwargs = {'linewidth':1, 'color':'k'}
         if tieline_plot_kwargs is not None:
             updated_tieline_plot_kwargs.update(tieline_plot_kwargs)
@@ -304,6 +306,10 @@ def dataplot(comps, phases, conds, datasets, tielines=True, ax=None, plot_kwargs
             if aliased_arg in plot_kwargs:
                 warnings.warn("'{0}' passed as plotting keyword argument to dataplot, but the alias '{1}' is already set to '{2}'. Use the full version of the keyword argument '{1}' to override the default.".format(aliased_arg, actual_arg, scatter_kwargs.get(actual_arg)))
         scatter_kwargs.update(plot_kwargs)
+
+        # Only T supported
+        if y is not v.T:
+            raise ValueError("Only fixed temperature currently supported for triangular projections")
 
         eq_dict = ravel_zpf_values(desired_data, [x, y], {'T': conds[v.T]})
 

espei/core_utils.py
@@ -163,14 +163,14 @@ def ravel_zpf_values(desired_data, independent_comps, conditions=None):
     ----------
     desired_data : List[Dataset]
         The selected data
-    independent_comps : list
+    independent_comps : List[str]
         List of indepdendent components. Used for mass balance component conversion
-    conditions : dict
+    conditions : Dict[str, Union[float, ArrayLike]]
         Conditions to filter for. Right now only considers fixed temperatures
 
     Returns
     -------
-    dict
+    Dict[int, List[List[Tuple[str, Dict[str, float], str]]] ]
         A dictonary of list of lists of tuples. Each dictionary key is the
         number of phases in equilibrium, e.g. a key "2" might have values
         [[(PHASE_NAME_1, {'C1': X1, 'C2': X2}, refkey), (PHASE_NAME_2, {'C1': X1, 'C2': X2}, refkey)], ...]
@@ -181,15 +181,24 @@ def ravel_zpf_values(desired_data, independent_comps, conditions=None):
     # integers are the number of equilibria in the phase that are lists of the individual points
     independent_comps = set(independent_comps)
     equilibria_dict = {}
-
     for data in desired_data:
         values = data['values']
-        T = ravel_conditions(data['values'], data['conditions']['T'], zpf=True)[0]
-
-        for equilbrium, temperature in zip(values, T):
+        data_temperatures = np.atleast_1d(data["conditions"]["T"])
+        if data_temperatures.squeeze().ndim == 0:
+            data_temperatures = np.asarray(len(data["values"]) * [data_temperatures[0]])
+        T = ravel_conditions(data['values'], data_temperatures.tolist(), zpf=True)[0]
+        data_pressures = np.atleast_1d(data["conditions"]["P"])
+        if data_pressures.squeeze().ndim == 0:
+            data_pressures = np.asarray(len(data["values"]) * [data_pressures[0]])
+        P = ravel_conditions(data['values'], data_pressures.tolist(), zpf=True)[0]
+        for equilbrium, temperature, pressure in zip(values, T, P):
             if conditions is not None:
                 # make sure that the conditions match
-                if temperature != conditions['T']:
+                temperature_condition = conditions.get('T')
+                if temperature_condition is not None and temperature != temperature_condition:
+                    continue
+                pressure_condition = conditions.get('P')
+                if pressure_condition is not None and pressure != pressure_condition:
                     continue
             # go through each equilibrium phase and ravel it correctly
             this_equilibrium = []
@@ -200,6 +209,9 @@ def ravel_zpf_values(desired_data, independent_comps, conditions=None):
 
                 # fix up any independent component issues and fill out the component dict
                 comp_dict = {}
+                # always add T and P
+                comp_dict['T'] = temperature
+                comp_dict['P'] = pressure
                 # assume that there are len(independent_comps)+1 components
                 # therefore mass balance applies
                 if independent_comps != set(components):
@@ -211,9 +223,7 @@ def ravel_zpf_values(desired_data, independent_comps, conditions=None):
                         c = list(independent_comps.difference(set(components)))[0]
                         x = None if any([xx is None for xx in compositions]) else 1 - sum(compositions)
                     comp_dict[c] = x
-                if conditions is None:
-                    # assume the other condition is temperature:
-                    comp_dict['T'] = temperature
+
                 this_equilibrium.append((phase_name, comp_dict, data['reference']))
 
             # add this set of equilibrium phases to the correct key in the equilibria dict

tests/test_core_utils.py
@@ -1,11 +1,12 @@
 import numpy as np
 import tinydb
 
-from espei.core_utils import get_prop_data, filter_configurations, filter_temperatures, symmetry_filter
+from espei.core_utils import get_prop_data, filter_configurations, filter_temperatures, symmetry_filter, ravel_zpf_values
 from espei.datasets import recursive_map
 from espei.sublattice_tools import recursive_tuplify
 from espei.utils import PickleableTinyDB, MemoryStorage
 from espei.error_functions.non_equilibrium_thermochemical_error import get_prop_samples
+from .testing_data import dataset_multi_valid_ternary, dataset_multi_valid_ternary_fixedT_manyP, dataset_multi_valid_ternary_manyT_manyP
 
 
 def test_get_data_for_a_minimal_example():
@@ -149,3 +150,44 @@ def test_get_prop_samples_broadcasts_weights_correctly():
     print(calculate_dict)
     assert calculate_dict["values"].shape == (16,)
     assert calculate_dict["values"].size == len(calculate_dict["weights"])
+
+
+def test_ravel_zpf_values_for_T_and_P_conditions():
+    """Temperature and pressure conditions can be acquired from ZPF data"""
+
+    # 3 temperatures, 1 pressure
+    ravelled_zpf_data = ravel_zpf_values([dataset_multi_valid_ternary], ["CR", "NI"])
+    print(ravelled_zpf_data)
+    two_phase_equilibria = ravelled_zpf_data[2]
+    assert [len(phase_compositions) for phase_compositions in two_phase_equilibria] == [2, 2, 2]
+    # 3 sets of equilibria, where each has two phase compositions with conditions
+    assert np.all(np.asarray([phase_comp_conds["P"] for _, phase_comp_conds, _ in two_phase_equilibria[0]]) == np.asarray([101325.0, 101325.0]))
+    assert np.all(np.asarray([phase_comp_conds["P"] for _, phase_comp_conds, _ in two_phase_equilibria[1]]) == np.asarray([101325.0, 101325.0]))
+    assert np.all(np.asarray([phase_comp_conds["P"] for _, phase_comp_conds, _ in two_phase_equilibria[2]]) == np.asarray([101325.0, 101325.0]))
+    assert np.all(np.asarray([phase_comp_conds["T"] for _, phase_comp_conds, _ in two_phase_equilibria[0]]) == np.asarray([1348, 1348]))
+    assert np.all(np.asarray([phase_comp_conds["T"] for _, phase_comp_conds, _ in two_phase_equilibria[1]]) == np.asarray([1176, 1176]))
+    assert np.all(np.asarray([phase_comp_conds["T"] for _, phase_comp_conds, _ in two_phase_equilibria[2]]) == np.asarray([977, 977]))
+
+    # 1 temperature, 3 pressures
+    ravelled_zpf_data = ravel_zpf_values([dataset_multi_valid_ternary_fixedT_manyP], ["CR", "NI"])
+    two_phase_equilibria = ravelled_zpf_data[2]
+    assert [len(phase_compositions) for phase_compositions in two_phase_equilibria] == [2, 2, 2]
+    # 3 sets of equilibria, where each has two phase compositions with conditions
+    assert np.all(np.asarray([phase_comp_conds["P"] for _, phase_comp_conds, _ in two_phase_equilibria[0]]) == np.asarray([1e5, 1e5]))
+    assert np.all(np.asarray([phase_comp_conds["P"] for _, phase_comp_conds, _ in two_phase_equilibria[1]]) == np.asarray([2e5, 2e5]))
+    assert np.all(np.asarray([phase_comp_conds["P"] for _, phase_comp_conds, _ in two_phase_equilibria[2]]) == np.asarray([3e5, 3e5]))
+    assert np.all(np.asarray([phase_comp_conds["T"] for _, phase_comp_conds, _ in two_phase_equilibria[0]]) == np.asarray([1000.0, 1000.0]))
+    assert np.all(np.asarray([phase_comp_conds["T"] for _, phase_comp_conds, _ in two_phase_equilibria[1]]) == np.asarray([1000.0, 1000.0]))
+    assert np.all(np.asarray([phase_comp_conds["T"] for _, phase_comp_conds, _ in two_phase_equilibria[2]]) == np.asarray([1000.0, 1000.0]))
+
+    # 3 temperatures, 3 pressures
+    ravelled_zpf_data = ravel_zpf_values([dataset_multi_valid_ternary_manyT_manyP], ["CR", "NI"])
+    two_phase_equilibria = ravelled_zpf_data[2]
+    assert [len(phase_compositions) for phase_compositions in two_phase_equilibria] == [2, 2, 2]
+    # 3 sets of equilibria, where each has two phase compositions with conditions
+    assert np.all(np.asarray([phase_comp_conds["P"] for _, phase_comp_conds, _ in two_phase_equilibria[0]]) == np.asarray([1e5, 1e5]))
+    assert np.all(np.asarray([phase_comp_conds["P"] for _, phase_comp_conds, _ in two_phase_equilibria[1]]) == np.asarray([2e5, 2e5]))
+    assert np.all(np.asarray([phase_comp_conds["P"] for _, phase_comp_conds, _ in two_phase_equilibria[2]]) == np.asarray([3e5, 3e5]))
+    assert np.all(np.asarray([phase_comp_conds["T"] for _, phase_comp_conds, _ in two_phase_equilibria[0]]) == np.asarray([1348, 1348]))
+    assert np.all(np.asarray([phase_comp_conds["T"] for _, phase_comp_conds, _ in two_phase_equilibria[1]]) == np.asarray([1176, 1176]))
+    assert np.all(np.asarray([phase_comp_conds["T"] for _, phase_comp_conds, _ in two_phase_equilibria[2]]) == np.asarray([977, 977]))

tests/test_datasets.py
@@ -2,7 +2,7 @@ import pytest
 import numpy as np
 from espei.datasets import DatasetError, check_dataset, clean_dataset, apply_tags
 
-from .testing_data import CU_MG_EXP_ACTIVITY, CU_MG_DATASET_THERMOCHEMICAL_STRING_VALUES, CU_MG_DATASET_ZPF_STRING_VALUES, LI_SN_LIQUID_DATA
+from .testing_data import CU_MG_EXP_ACTIVITY, CU_MG_DATASET_THERMOCHEMICAL_STRING_VALUES, CU_MG_DATASET_ZPF_STRING_VALUES, LI_SN_LIQUID_DATA, dataset_multi_valid_ternary
 from .fixtures import datasets_db
 
 dataset_single_valid = {
@@ -66,20 +66,6 @@ dataset_multi_valid = {
     ],
 }
 
-dataset_multi_valid_ternary = {
-    "components": ["AL", "CR", "NI", "VA"],
-    "phases": ["AL3NI2", "BCC_B2"],
-    "conditions": {
-        "P": 101325,
-        "T": [1348, 1176, 977]
-    },
-    "output": "ZPF",
-    "values": [
-        [["AL3NI2", ["CR", "NI"], [0.2, 0.4083]], ["BCC_B2", ["CR", "NI"], [None, None]]],
-        [["AL3NI2", ["CR", "NI"], [0.2, 0.4114]], ["BCC_B2", ["CR", "NI"], [0.2, 0.4456]]],
-        [["AL3NI2", ["CR", "NI"], [0.2, 0.4114]], ["BCC_B2", ["CR", "NI"], [0.2, 0.4532]]]
-    ],
-}
 
 dataset_multi_misaligned = {
     "components": ["AL", "NI"],

tests/testing_data.py
@@ -1388,3 +1388,54 @@ SGTE91_PURE_ELEMENTS = (
       'SM', 'SN', 'SR', 'TA', 'TB', 'TC', 'TE', 'TH', 'TI', 'TL', 'TM', 'U',
       'V', 'W', 'Y', 'YB', 'ZN', 'ZR',
 )
+
+
+dataset_multi_valid_ternary = {
+    "components": ["AL", "CR", "NI", "VA"],
+    "phases": ["AL3NI2", "BCC_B2"],
+    "conditions": {
+        "P": 101325,
+        "T": [1348, 1176, 977]
+    },
+    "output": "ZPF",
+    "values": [
+        [["AL3NI2", ["CR", "NI"], [0.2, 0.4083]], ["BCC_B2", ["CR", "NI"], [None, None]]],
+        [["AL3NI2", ["CR", "NI"], [0.2, 0.4114]], ["BCC_B2", ["CR", "NI"], [0.2, 0.4456]]],
+        [["AL3NI2", ["CR", "NI"], [0.2, 0.4114]], ["BCC_B2", ["CR", "NI"], [0.2, 0.4532]]]
+    ],
+    "reference": "testreference",
+}
+
+
+dataset_multi_valid_ternary_fixedT_manyP = {
+    "components": ["AL", "CR", "NI", "VA"],
+    "phases": ["AL3NI2", "BCC_B2"],
+    "conditions": {
+        "P": [1e5, 2e5, 3e5],
+        "T": [1000.0]
+    },
+    "output": "ZPF",
+    "values": [
+        [["AL3NI2", ["CR", "NI"], [0.2, 0.4083]], ["BCC_B2", ["CR", "NI"], [None, None]]],
+        [["AL3NI2", ["CR", "NI"], [0.2, 0.4114]], ["BCC_B2", ["CR", "NI"], [0.2, 0.4456]]],
+        [["AL3NI2", ["CR", "NI"], [0.2, 0.4114]], ["BCC_B2", ["CR", "NI"], [0.2, 0.4532]]]
+    ],
+    "reference": "testreference",
+}
+
+
+dataset_multi_valid_ternary_manyT_manyP = {
+    "components": ["AL", "CR", "NI", "VA"],
+    "phases": ["AL3NI2", "BCC_B2"],
+    "conditions": {
+        "P": [1e5, 2e5, 3e5],
+        "T": [1348, 1176, 977]
+    },
+    "output": "ZPF",
+    "values": [
+        [["AL3NI2", ["CR", "NI"], [0.2, 0.4083]], ["BCC_B2", ["CR", "NI"], [None, None]]],
+        [["AL3NI2", ["CR", "NI"], [0.2, 0.4114]], ["BCC_B2", ["CR", "NI"], [0.2, 0.4456]]],
+        [["AL3NI2", ["CR", "NI"], [0.2, 0.4114]], ["BCC_B2", ["CR", "NI"], [0.2, 0.4532]]]
+    ],
+    "reference": "testreference",
+}

commit 0e7e092f10745c388ebaffeadbb43b29a57ab9f6

FIX: Fix ternary projection dataplots (#241)
Isothermal ternaries were broken by #240 by a bad error check that the `y` variable was temperature. For ternary triangular projections, both `x` and `y` should be molar quantities (i.e. `v.X()`). Both temperature and pressure need to be fixed scalars and therefore the filtering for `ravel_zpf_values` should be filtering against both potentials.

espei/plot.py
@@ -307,11 +307,8 @@ def dataplot(comps, phases, conds, datasets, tielines=True, ax=None, plot_kwargs
                 warnings.warn("'{0}' passed as plotting keyword argument to dataplot, but the alias '{1}' is already set to '{2}'. Use the full version of the keyword argument '{1}' to override the default.".format(aliased_arg, actual_arg, scatter_kwargs.get(actual_arg)))
         scatter_kwargs.update(plot_kwargs)
 
-        # Only T supported
-        if y is not v.T:
-            raise ValueError("Only fixed temperature currently supported for triangular projections")
-
-        eq_dict = ravel_zpf_values(desired_data, [x, y], {'T': conds[v.T]})
+        # Axes must both be molar quantities, so assume that P and T potentials are not fixed
+        eq_dict = ravel_zpf_values(desired_data, [x, y], {'T': conds[v.T], 'P': conds[v.P]})
 
         # two phase
         updated_tieline_plot_kwargs = {'linewidth':1, 'color':'k'}

tests/test_plotting.py
@@ -28,6 +28,21 @@ def test_dataplot_runs(datasets_db):
     plt.close(fig)
 
 
+def test_dataplot_runs_ternary_isothermal(datasets_db):
+    """Test that dataplot runs without an error for a ternary isothermal case."""
+
+    datasets_db.insert(A_B_C_DATASET_TERNARY_PHASE_EQUILIBRIA)
+
+    comps = ['A', 'B', 'C', 'VA']
+    phases = ['PHASE_1', 'PHASE_2']
+    conds = {v.P: 101325, v.T: 300.0, v.X('B'): (0, 1, 0.01), v.X('C'): (0, 1, 0.01)}
+    fig = plt.figure()
+    dataplot(comps, phases, conds, datasets_db)
+    # fig.savefig('test_dataplot_runs-figure.png')
+    plt.close(fig)
+
+
+
 def test_plot_interaction_runs(datasets_db):
     """Test that plot_interaction runs without an error."""
     dbf = Database(CU_MG_TDB)