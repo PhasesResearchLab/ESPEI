204ef1bc8cf73eba4585bc50941da922eb343542
Brandon Bocklund <bocklund1@llnl.gov>
2023-11-30 18:44:07 -0800

    Remove elastic model and fitting description

--- espei/parameter_selection/fitting_descriptions.py
+++ espei/parameter_selection/fitting_descriptions.py
@@ -22,20 +22,4 @@ class ModelFittingDescription():
 
 
 molar_volume_fit_desc = ModelFittingDescription([StepV0, StepLogVA])
-
-class ElasticModel(Model):
-    def build_phase(self, dbe):
-        phase = dbe.phases[self.phase_name]
-        param_search = dbe.search
-        for prop in ['C11', 'C12', 'C44']:
-            prop_param_query = (
-                (tinydb.where('phase_name') == phase.name) & \
-                (tinydb.where('parameter_type') == prop) & \
-                (tinydb.where('constituent_array').test(self._array_validity))
-                )
-            prop_val = self.redlich_kister_sum(phase, param_search, prop_param_query).subs(dbe.symbols)
-            setattr(self, prop, prop_val)
-
-elastic_fitting_description = ModelFittingDescription([StepElasticC11, StepElasticC12, StepElasticC44], model=ElasticModel)
-
 gibbs_energy_fitting_description = ModelFittingDescription([StepCPM, StepSM, StepHM])
