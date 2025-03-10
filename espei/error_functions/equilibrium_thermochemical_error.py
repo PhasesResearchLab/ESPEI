ca40de7cdfe097bfeed8c5b55500548c669a0d60
nury12n <48889915+nury12n@users.noreply.github.com>
2025-03-10 11:11:32 -0700

    ENH/FIX: Mobility fitting compatibility and context futures (#264)
    - Add compatibility with Kawin-based mobility fitting for diffusion data by changing the thermochemical equilibrium residual to ignore any datasets with `TRACER` or `DIFF` in the `output` field, as those are handled by Kawin.
    - Provide the ESPEI residual function context to dask workers as futures instead of providing every the context every iteration. Submitting futures occurs during the first iteration and anytime a worker is restarted. This should help mitigate the memory leak caused by pickling and unpickling the context (see #230).

--- espei/error_functions/equilibrium_thermochemical_error.py
+++ espei/error_functions/equilibrium_thermochemical_error.py
@@ -160,6 +160,7 @@ def get_equilibrium_thermochemical_data(dbf: Database, comps: Sequence[str],
         # data that isn't ZPF or non-equilibrium thermochemical
         (where('output') != 'ZPF') & (~where('solver').exists()) &
         (where('output').test(lambda x: 'ACR' not in x)) &  # activity data not supported yet
+        (where('output').test(lambda x: 'DIFF' not in x)) & (where('output').test(lambda x: 'TRACER' not in x)) & # ignore diffusivity
         (where('components').test(lambda x: set(x).issubset(comps))) &
         (where('phases').test(lambda x: set(x).issubset(set(phases))))
     )
